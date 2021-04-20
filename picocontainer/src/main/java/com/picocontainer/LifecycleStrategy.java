47a732e9e86f0906da6e068c66659184b93bdaaf
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-20 21:24:35 +0200

    refactor: cleanup LifecycleStrategy

--- picocontainer/src/main/java/com/picocontainer/LifecycleStrategy.java
+++ picocontainer/src/main/java/com/picocontainer/LifecycleStrategy.java
@@ -16,50 +16,49 @@ package com.picocontainer;
  * @author Peter Royal
  * @author J&ouml;rg Schaible
  * @author Mauro Talevi
- * @see com.picocontainer.Startable
- * @see com.picocontainer.Disposable
+ * @see Startable
+ * @see Disposable
  */
 public interface LifecycleStrategy {
+  /**
+   * Invoke the "start" method on the component instance if this is startable.
+   * It is up to the implementation of the strategy what "start" and "startable" means.
+   *
+   * @param component the instance of the component to start
+   */
+  void start(final Object component);
 
-    /**
-     * Invoke the "start" method on the component instance if this is startable.
-     * It is up to the implementation of the strategy what "start" and "startable" means.
-     *
-     * @param component the instance of the component to start
-     */
-    void start(Object component);
+  /**
+   * Invoke the "stop" method on the component instance if this is stoppable.
+   * It is up to the implementation of the strategy what "stop" and "stoppable" means.
+   *
+   * @param component the instance of the component to stop
+   */
+  void stop(final Object component);
 
-    /**
-     * Invoke the "stop" method on the component instance if this is stoppable.
-     * It is up to the implementation of the strategy what "stop" and "stoppable" means.
-     *
-     * @param component the instance of the component to stop
-     */
-    void stop(Object component);
+  /**
+   * Invoke the "dispose" method on the component instance if this is disposable.
+   * It is up to the implementation of the strategy what "dispose" and "disposable" means.
+   *
+   * @param component the instance of the component to dispose
+   */
+  void dispose(final Object component);
 
-    /**
-     * Invoke the "dispose" method on the component instance if this is disposable.
-     * It is up to the implementation of the strategy what "dispose" and "disposable" means.
-     *
-     * @param component the instance of the component to dispose
-     */
-    void dispose(Object component);
+  /**
+   * Test if a component instance has a lifecycle.
+   *
+   * @param type the component's type
+   * @return {@code true} if the component has a lifecycle
+   */
+  boolean hasLifecycle(final Class<?> type);
 
-    /**
-     * Test if a component instance has a lifecycle.
-     * @param type the component's type
-     *
-     * @return <code>true</code> if the component has a lifecycle
-     */
-    boolean hasLifecycle(Class<?> type);
-
-    /**
-     * Is a component eager (not lazy) in that it should start when start() or equivalent is called,
-     * or lazy (it will only start on first getComponent() ).
-     * The default is the first of those two.
-     *
-     * @param adapter
-     * @return true if lazy, false if not lazy
-     */
-    boolean isLazy(ComponentAdapter<?> adapter);
+  /**
+   * Is a component eager (it will start when {@code start()} or equivalent is called),
+   * or lazy (it will only start on the first {@code getComponent()})?
+   * The default is the first of those two.
+   *
+   * @param adapter
+   * @return {@code true} if lazy, {@code false} if eager
+   */
+  boolean isLazy(final ComponentAdapter<?> adapter);
 }
