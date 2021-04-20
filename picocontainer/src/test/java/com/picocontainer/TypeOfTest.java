66293fb7a2049380f09bb4098d7b29452f0db86f
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-20 19:04:38 +0200

    test: update TypeOfTest to JUnit 5

--- picocontainer/src/test/java/com/picocontainer/TypeOfTest.java
+++ picocontainer/src/test/java/com/picocontainer/TypeOfTest.java
@@ -1,44 +1,39 @@
 package com.picocontainer;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertSame;
-import static org.junit.Assert.assertNotSame;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import java.awt.Color;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import org.junit.Test;
-
-import com.picocontainer.TypeOf;
 
 public class TypeOfTest {
 
     @Test
     public void typeErasureSidesteppedWithGenericSubclass() {
         TypeOf<Set<Color>> blort = new TypeOf<Set<Color>>(){};
-        assertEquals("java.util.Set<java.awt.Color>", blort.getType().toString());
+        Assertions.assertEquals("java.util.Set<java.awt.Color>", blort.getType().toString());
     }
 
     @Test
     public void canUseRealClassesToo() {
         TypeOf<Color> blort = new TypeOf<Color>(){};
-        assertEquals(Color.class, blort.getType());
+        Assertions.assertEquals(Color.class, blort.getType());
     }
 
     @Test
     public void factoryMethodMakesTypeOfForRegularNonGenericClasses() {
         TypeOf<Color> blort = TypeOf.fromClass(Color.class);
-        assertSame(Color.class, blort.getType());
+        Assertions.assertSame(Color.class, blort.getType());
     }
 
     @Test
     public void typeOflooksGoodInAMethodDeclaration() {
         StringBuffer sb = new StringBuffer();
         Set<List<Color>> setOfListOfColors = getComponent(new TypeOf<List<Color>>(){}, sb);
-        assertEquals("java.util.List<java.awt.Color>", sb.toString());
+        Assertions.assertEquals("java.util.List<java.awt.Color>", sb.toString());
         List<Color> reds = new ArrayList<Color>();
         reds.add(Color.red);
         reds.add(Color.orange);
@@ -55,7 +50,7 @@ public class TypeOfTest {
     public void oldWaylooksGoodInAMethodDeclaration() {
         StringBuffer sb = new StringBuffer();
         Set<Color> setOfColors = getComponent(Color.class, sb);
-        assertEquals("class java.awt.Color", sb.toString());
+        Assertions.assertEquals("class java.awt.Color", sb.toString());
 
         setOfColors.add(Color.red);
         setOfColors.add(Color.orange);
@@ -69,13 +64,13 @@ public class TypeOfTest {
 
     @Test
     public void hashCodeWorks() {
-        assertEquals(TypeOf.fromClass(Set.class).hashCode(), TypeOf.fromClass(Set.class).hashCode());
+        Assertions.assertEquals(TypeOf.fromClass(Set.class).hashCode(), TypeOf.fromClass(Set.class).hashCode());
     }
 
     @Test
     public void equalsWorks() {
-        assertNotSame(TypeOf.fromClass(Set.class), TypeOf.fromClass(Set.class));
-        assertEquals(TypeOf.fromClass(Set.class), TypeOf.fromClass(Set.class));
+        Assertions.assertNotSame(TypeOf.fromClass(Set.class), TypeOf.fromClass(Set.class));
+        Assertions.assertEquals(TypeOf.fromClass(Set.class), TypeOf.fromClass(Set.class));
     }
 
 }
