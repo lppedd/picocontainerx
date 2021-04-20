1ccae40dc800d0be528cca87a57fa9e84597ae41
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-20 21:21:09 +0200

    refactor: cleanup Key

--- picocontainer/src/main/java/com/picocontainer/Key.java
+++ picocontainer/src/main/java/com/picocontainer/Key.java
@@ -11,63 +11,56 @@ package com.picocontainer;
 
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
+import java.util.Objects;
 
-/** @author Paul Hammant */
+/**
+ * TODO
+ *
+ * @author Paul Hammant
+ */
 @SuppressWarnings("serial")
 public class Key<T> implements Serializable {
+  private final Class<T> type;
+  private final Class<? extends Annotation> annotation;
 
-	private final Class<T> type;
-    private final Class<? extends Annotation> annotation;
+  public Key(final Class<T> type, final Class<? extends Annotation> annotation) {
+    this.type = type;
+    this.annotation = annotation;
+  }
 
-    public Key(final Class<T> type, final Class<? extends Annotation> annotation) {
-        this.type = type;
-        this.annotation = annotation;
+  public Class<T> getType() {
+    return type;
+  }
+
+  public Class<? extends Annotation> getAnnotation() {
+    return annotation;
+  }
+
+  @Override
+  public String toString() {
+    return type.getName() + ":" + annotation.getName();
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
     }
 
-    public Class<T> getType() {
-        return type;
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
 
-    public Class<? extends Annotation> getAnnotation() {
-        return annotation;
-    }
+    final Key<?> key = (Key<?>) o;
+    return annotation.equals(key.annotation) && type.equals(key.type);
+  }
 
-    @Override
-	public String toString() {
-        return type.getName() + ":" + annotation.getName();
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(type, annotation);
+  }
 
-    @Override
-	public boolean equals(final Object o) {
-        if (this == o) {
-			return true;
-		}
-        if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-
-        Key<?> key = (Key<?>)o;
-
-        if (!annotation.equals(key.annotation)) {
-			return false;
-		}
-        if (!type.equals(key.type)) {
-			return false;
-		}
-
-        return true;
-    }
-
-    @Override
-	public int hashCode() {
-        int result;
-        result = type.hashCode();
-        result = 31 * result + annotation.hashCode();
-        return result;
-    }
-
-    public static <T> Key<T> annotatedKey(final Class<T> type, final Class<? extends Annotation> annotation) {
-        return new Key<T>(type, annotation);
-    }
-
+  public static <T> Key<T> annotatedKey(final Class<T> type, final Class<? extends Annotation> annotation) {
+    return new Key<>(type, annotation);
+  }
 }
