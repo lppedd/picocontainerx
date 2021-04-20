d861c88659951d7ec3eaac15f40210858f4917df
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-20 20:19:42 +0200

    refactor: cleanup ComponentFactory

--- picocontainer/src/main/java/com/picocontainer/ComponentFactory.java
+++ picocontainer/src/main/java/com/picocontainer/ComponentFactory.java
@@ -16,65 +16,61 @@ import com.picocontainer.parameters.MethodParameters;
 import java.util.Properties;
 
 /**
- * <p/>
- * A component factory is responsible for creating
- * {@link ComponentAdapter} component adapters. The main use of the component factory is
- * inside {@link DefaultPicoContainer#DefaultPicoContainer(ComponentFactory)}, where it can
- * be used to customize the default component adapter that is used when none is specified
- * explicitly.
- * </p>
+ * A component factory is responsible for creating {@link ComponentAdapter} component adapters.
+ * The main use of the component factory is inside {@link DefaultPicoContainer#DefaultPicoContainer(ComponentFactory)},
+ * where it can be used to customize the default component adapter that is used when none is specified explicitly.
  *
  * @author Paul Hammant
  * @author Mauro Talevi
  * @author Jon Tirs&eacute;n
  */
 public interface ComponentFactory {
+  /**
+   * Create a new component adapter based on the specified arguments.
+   *
+   * @param monitor the component monitor
+   * @param lifecycle te lifecycle strategy
+   * @param componentProps the component properties
+   * @param key the key to be associated with this adapter. This
+   * value should be returned from a call to
+   * {@link ComponentAdapter#getComponentKey()} on the created
+   * adapter.
+   * @param impl the implementation class to be associated
+   * with this adapter. This value should be returned from a call
+   * to {@link ComponentAdapter#getComponentImplementation()} on
+   * the created adapter. Should not be null.
+   * @param constructorParams TODO
+   * @param fieldParams TODO
+   * @param methodParams TODO
+   * @return a new component adapter based on the specified arguments. Should
+   * not return null.
+   */
+  <T> ComponentAdapter<T> createComponentAdapter(
+      final ComponentMonitor monitor,
+      final LifecycleStrategy lifecycle,
+      final Properties componentProps,
+      final Object key,
+      final Class<T> impl,
+      final ConstructorParameters constructorParams,
+      final FieldParameters[] fieldParams,
+      final MethodParameters[] methodParams
+  );
 
-    /**
-     * Create a new component adapter based on the specified arguments.
-     *
-     * @param monitor the component monitor
-     * @param lifecycle te lifecycle strategy
-     * @param componentProps the component properties
-     * @param key the key to be associated with this adapter. This
-     *            value should be returned from a call to
-     *            {@link ComponentAdapter#getComponentKey()} on the created
-     *            adapter.
-     * @param impl the implementation class to be associated
-     *            with this adapter. This value should be returned from a call
-     *            to {@link ComponentAdapter#getComponentImplementation()} on
-     *            the created adapter. Should not be null.
-     * @param constructorParams TODO
-     * @param fieldParams TODO
-     * @param methodParams TODO
-     * @return a new component adapter based on the specified arguments. Should
-     *         not return null.
-     * @throws PicoCompositionException if the creation of the component adapter
-     *             results in a {@link PicoCompositionException}.
-     * @return The component adapter
-     */
-    <T> ComponentAdapter<T> createComponentAdapter(ComponentMonitor monitor,
-                                            LifecycleStrategy lifecycle,
-                                            Properties componentProps,
-                                            Object key,
-                                            Class<T> impl,
-                                            ConstructorParameters constructorParams, FieldParameters[] fieldParams, MethodParameters[] methodParams) throws PicoCompositionException;
+  /**
+   * Verification for the {@code ComponentFactory} - subject to implementation.
+   *
+   * @param container the {@link PicoContainer} that is used for verification.
+   */
+  void verify(final PicoContainer container);
 
-    /**
-     * Verification for the ComponentFactory - subject to implementation.
-     *
-     * @param container the {@link PicoContainer}, that is used for verification.
-     * @throws PicoCompositionException if one or more dependencies cannot be resolved.
-     */
-    void verify(PicoContainer container);
+  /**
+   * Accepts a visitor for this {@code ComponentFactory}.
+   * The method is normally called by visiting a {@link PicoContainer},
+   * that cascades the visitor also down to all its ComponentFactory instances.
+   *
+   * @param visitor the visitor.
+   */
+  void accept(final PicoVisitor visitor);
 
-    /**
-     * Accepts a visitor for this ComponentFactory. The method is normally called by visiting a {@link PicoContainer}, that
-     * cascades the visitor also down to all its ComponentFactory instances.
-     *
-     * @param visitor the visitor.
-     */
-    void accept(PicoVisitor visitor);
-    
-    void dispose();
+  void dispose();
 }
