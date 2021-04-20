8ac77ee616b65e2bcf4fb22b15a9c92c6fde2f01
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-20 21:02:44 +0200

    refactor: cleanup Converting

--- picocontainer/src/main/java/com/picocontainer/Converting.java
+++ picocontainer/src/main/java/com/picocontainer/Converting.java
@@ -1,17 +1,14 @@
 package com.picocontainer;
 
-
 /**
  * Interface for containers that can handle string-to-object conversion in object parameters.
+ *
  * @author Paul Hammant
  */
+@SuppressWarnings("InterfaceMayBeAnnotatedFunctional")
 public interface Converting {
-
-    /**
-     * Retrieve the set of converters for transforming string parameters
-     * into objects.
-     * @return converter set instance.
-     */
-    Converters getConverters();
-
+  /**
+   * Retrieve the set of converters for transforming string parameters into objects.
+   */
+  Converters getConverters();
 }
