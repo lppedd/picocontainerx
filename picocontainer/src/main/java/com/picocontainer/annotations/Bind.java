a490c01782ec6ef0c68b5dc6ec5fb0b1af4e39ae
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-20 22:26:30 +0200

    refactor: cleanup Bind

--- picocontainer/src/main/java/com/picocontainer/annotations/Bind.java
+++ picocontainer/src/main/java/com/picocontainer/annotations/Bind.java
@@ -5,8 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/** @author Paul Hammant */
+/**
+ * @author Paul Hammant
+ */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.ANNOTATION_TYPE })
-public @interface Bind {
-}
+@Target(ElementType.ANNOTATION_TYPE)
+public @interface Bind {}
