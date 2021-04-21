3b9cda864aec03fa7bd2c58d5378cc67bdf53d4f
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 11:02:16 +0200

    refactor: cleanup ClassLoadingPicoContainer

--- picocontainer/src/main/java/com/picocontainer/classname/ClassLoadingPicoContainer.java
+++ picocontainer/src/main/java/com/picocontainer/classname/ClassLoadingPicoContainer.java
@@ -13,43 +13,40 @@ import com.picocontainer.PicoContainer;
 import java.net.URL;
 
 /**
- * A ClassLoadingPicoContainer extends PicoContainer with classloader juggling capability
+ * Extends {@link PicoContainer} with classloader juggling capabilities.
  *
  * @author Paul Hammant
  * @author Aslak Helles&oslash;y
  */
 public interface ClassLoadingPicoContainer extends MutablePicoContainer {
+  /**
+   * Adds a new URL that will be used in classloading.
+   *
+   * @param url url of the jar to find components in
+   * @return {@code ClassPathElement} to add permissions to (subject to security policy)
+   */
+  ClassPathElement addClassLoaderURL(final URL url);
 
-    /**
-     * Adds a new URL that will be used in classloading
-     *
-     * @param url url of the jar to find components in.
-     * @return ClassPathElement to add permissions to (subject to security
-     *         policy)
-     */
-    ClassPathElement addClassLoaderURL(URL url);
+  /**
+   * Returns the {@link ClassLoader} that is the aggregate of the URLs added.
+   *
+   * @return A ClassLoader
+   */
+  ClassLoader getComponentClassLoader();
 
-    /**
-     * Returns class loader that is the aggregate of the URLs added.
-     *
-     * @return A ClassLoader
-     */
-    ClassLoader getComponentClassLoader();
+  /**
+   * Makes a child container with a given name
+   *
+   * @param name the container name
+   * @return The ScriptedPicoContainer
+   */
+  ClassLoadingPicoContainer makeChildContainer(final String name);
 
-    /**
-     * Make a child container with a given name
-     *
-     * @param name the container name
-     * @return The ScriptedPicoContainer
-     */
-    ClassLoadingPicoContainer makeChildContainer(String name);
-
-    /**
-     * Addes a child container with a given name
-     *
-     * @param name the container name
-     * @param child the child PicoContainer
-     */
-    ClassLoadingPicoContainer addChildContainer(String name, PicoContainer child);
-
+  /**
+   * Adds a child container with a given name
+   *
+   * @param name the container name
+   * @param child the child PicoContainer
+   */
+  ClassLoadingPicoContainer addChildContainer(final String name, final PicoContainer child);
 }
