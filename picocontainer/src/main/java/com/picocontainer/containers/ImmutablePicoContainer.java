1e098539a1053f35b30423784fd8f2cc0147f595
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 17:15:47 +0200

    refactor: cleanup ImmutablePicoContainer

--- picocontainer/src/main/java/com/picocontainer/containers/ImmutablePicoContainer.java
+++ picocontainer/src/main/java/com/picocontainer/containers/ImmutablePicoContainer.java
@@ -10,7 +10,13 @@
 package com.picocontainer.containers;
 
 import com.googlecode.jtype.Generic;
-import com.picocontainer.*;
+import com.picocontainer.ComponentAdapter;
+import com.picocontainer.ComponentAdapter.NOTHING;
+import com.picocontainer.Converters;
+import com.picocontainer.Converting;
+import com.picocontainer.NameBinding;
+import com.picocontainer.PicoContainer;
+import com.picocontainer.PicoVisitor;
 import com.picocontainer.converters.ConvertsNothing;
 
 import java.io.Serializable;
@@ -20,134 +26,172 @@ import java.util.Collection;
 import java.util.List;
 
 /**
-* wrap pico container to achieve immutability
- * Typically its used to mock a parent container.
+ * Wraps a {@link PicoContainer} to achieve immutability.
+ * Typically used to mock a parent container.
  *
  * @author Konstantin Pribluda
  */
 @SuppressWarnings("serial")
 public final class ImmutablePicoContainer implements PicoContainer, Converting, Serializable {
+  private final PicoContainer delegate;
 
-    private final PicoContainer delegate;
-
-    public ImmutablePicoContainer(final PicoContainer delegate) {
-        if (delegate == null) {
-            throw new NullPointerException();
-        }
-        this.delegate = delegate;
+  public ImmutablePicoContainer(final PicoContainer delegate) {
+    if (delegate == null) {
+      throw new NullPointerException();
     }
 
-    public Object getComponent(final Object keyOrType) {
-        return getComponentInto(keyOrType, ComponentAdapter.NOTHING.class);
+    this.delegate = delegate;
+  }
+
+  @Override
+  public Object getComponent(final Object keyOrType) {
+    return getComponentInto(keyOrType, NOTHING.class);
+  }
+
+  @Override
+  public Object getComponentInto(final Object keyOrType, final Type into) {
+    return delegate.getComponentInto(keyOrType, into);
+  }
+
+  @Override
+  public <T> T getComponent(final Class<T> componentType) {
+    return delegate.getComponent(Generic.get(componentType));
+  }
+
+  @Override
+  public <T> T getComponent(final Generic<T> componentType) {
+    return delegate.getComponent(componentType);
+  }
+
+  @Override
+  public <T> T getComponentInto(final Class<T> componentType, final Type into) {
+    return delegate.getComponentInto(componentType, into);
+  }
+
+  @Override
+  public <T> T getComponentInto(final Generic<T> componentType, final Type into) {
+    return delegate.getComponentInto(componentType, into);
+  }
+
+  @Override
+  public <T> T getComponent(
+      final Class<T> componentType,
+      final Class<? extends Annotation> binding,
+      final Type into) {
+    return delegate.getComponent(componentType, binding, into);
+  }
+
+  @Override
+  public <T> T getComponent(final Class<T> componentType, final Class<? extends Annotation> binding) {
+    return delegate.getComponent(componentType, binding);
+  }
+
+  @Override
+  public List<Object> getComponents() {
+    return delegate.getComponents();
+  }
+
+  @Override
+  public PicoContainer getParent() {
+    return delegate.getParent();
+  }
+
+  @Override
+  public ComponentAdapter<?> getComponentAdapter(final Object key) {
+    return delegate.getComponentAdapter(key);
+  }
+
+  @Override
+  public <T> ComponentAdapter<T> getComponentAdapter(
+      final Class<T> componentType,
+      final NameBinding nameBinding) {
+    return delegate.getComponentAdapter(Generic.get(componentType), nameBinding);
+  }
+
+  @Override
+  public <T> ComponentAdapter<T> getComponentAdapter(
+      final Generic<T> componentType,
+      final NameBinding componentNameBinding) {
+    return delegate.getComponentAdapter(componentType, componentNameBinding);
+  }
+
+  @Override
+  public <T> ComponentAdapter<T> getComponentAdapter(
+      final Class<T> componentType,
+      final Class<? extends Annotation> binding) {
+    return delegate.getComponentAdapter(Generic.get(componentType), binding);
+  }
+
+  @Override
+  public <T> ComponentAdapter<T> getComponentAdapter(
+      final Generic<T> componentType,
+      final Class<? extends Annotation> binding) {
+    return delegate.getComponentAdapter(componentType, binding);
+  }
+
+  @Override
+  public Collection<ComponentAdapter<?>> getComponentAdapters() {
+    return delegate.getComponentAdapters();
+  }
+
+  @Override
+  public <T> List<ComponentAdapter<T>> getComponentAdapters(final Class<T> componentType) {
+    return delegate.getComponentAdapters(Generic.get(componentType));
+  }
+
+  @Override
+  public <T> List<ComponentAdapter<T>> getComponentAdapters(final Generic<T> componentType) {
+    return delegate.getComponentAdapters(componentType);
+  }
+
+  @Override
+  public <T> List<ComponentAdapter<T>> getComponentAdapters(
+      final Class<T> componentType,
+      final Class<? extends Annotation> binding) {
+    return delegate.getComponentAdapters(Generic.get(componentType), binding);
+  }
+
+  @Override
+  public <T> List<ComponentAdapter<T>> getComponentAdapters(
+      final Generic<T> componentType,
+      final Class<? extends Annotation> binding) {
+    return delegate.getComponentAdapters(componentType, binding);
+  }
+
+  @Override
+  public <T> List<T> getComponents(final Class<T> componentType) {
+    return delegate.getComponents(componentType);
+  }
+
+  @Override
+  public final void accept(final PicoVisitor visitor) {
+    delegate.accept(visitor);
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    return obj == this
+        || obj != null && obj == delegate
+        || obj instanceof ImmutablePicoContainer && ((ImmutablePicoContainer) obj).delegate == delegate;
+  }
+
+  @Override
+  public int hashCode() {
+    return delegate.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return "[Immutable]:" + delegate;
+  }
+
+  @Override
+  public Converters getConverters() {
+    // noinspection SimplifiableIfStatement
+    if (delegate instanceof Converting) {
+      return ((Converting) delegate).getConverters();
     }
 
-    public Object getComponentInto(final Object keyOrType, final Type into) {
-        return delegate.getComponentInto(keyOrType, into);
-    }
-
-    public <T> T getComponent(final Class<T> componentType) {
-        return delegate.getComponent(Generic.get(componentType));
-    }
-
-    public <T> T getComponent(final Generic<T> componentType) {
-        return delegate.getComponent(componentType);
-    }
-
-    public <T> T getComponentInto(final Class<T> componentType, final Type into) {
-        return delegate.getComponentInto(componentType, into);
-    }
-
-    public <T> T getComponentInto(final Generic<T> componentType, final Type into) {
-        return delegate.getComponentInto(componentType, into);
-    }
-
-    public <T> T getComponent(final Class<T> componentType, final Class<? extends Annotation> binding, final Type into) {
-        return delegate.getComponent(componentType, binding, into);
-    }
-
-    public <T> T getComponent(final Class<T> componentType, final Class<? extends Annotation> binding) {
-        return delegate.getComponent(componentType, binding);
-    }
-
-    public List getComponents() {
-        return delegate.getComponents();
-    }
-
-    public PicoContainer getParent() {
-        return delegate.getParent();
-    }
-
-    public ComponentAdapter<?> getComponentAdapter(final Object key) {
-        return delegate.getComponentAdapter(key);
-    }
-
-    public <T> ComponentAdapter<T> getComponentAdapter(final Class<T> componentType, final NameBinding nameBinding) {
-        return delegate.getComponentAdapter(Generic.get(componentType), nameBinding);
-    }
-
-    public <T> ComponentAdapter<T> getComponentAdapter(final Generic<T> componentType, final NameBinding componentNameBinding) {
-        return delegate.getComponentAdapter(componentType, componentNameBinding);
-    }
-
-    public <T> ComponentAdapter<T> getComponentAdapter(final Class<T> componentType, final Class<? extends Annotation> binding) {
-        return delegate.getComponentAdapter(Generic.get(componentType), binding);
-    }
-
-    public <T> ComponentAdapter<T> getComponentAdapter(final Generic<T> componentType, final Class<? extends Annotation> binding) {
-        return delegate.getComponentAdapter(componentType, binding);
-    }
-
-    public Collection<ComponentAdapter<?>> getComponentAdapters() {
-        return delegate.getComponentAdapters();
-    }
-
-    public <T> List<ComponentAdapter<T>> getComponentAdapters(final Class<T> componentType) {
-        return delegate.getComponentAdapters(Generic.get(componentType));
-    }
-
-    public <T> List<ComponentAdapter<T>> getComponentAdapters(final Generic<T> componentType) {
-        return delegate.getComponentAdapters(componentType);
-    }
-
-    public <T> List<ComponentAdapter<T>> getComponentAdapters(final Class<T> componentType, final Class<? extends Annotation> binding) {
-        return delegate.getComponentAdapters(Generic.get(componentType), binding);
-    }
-
-    public <T> List<ComponentAdapter<T>> getComponentAdapters(final Generic<T> componentType, final Class<? extends Annotation> binding) {
-        return delegate.getComponentAdapters(componentType, binding);
-    }
-
-    public <T> List<T> getComponents(final Class<T> componentType) {
-        return delegate.getComponents(componentType);
-    }
-
-    public final void accept(final PicoVisitor visitor) {
-        // don't visit "this" its pointless.
-        delegate.accept(visitor);
-    }
-
-    @Override
-	public boolean equals(final Object obj) {
-        return obj == this
-               || (obj != null && obj == delegate)
-               || (obj instanceof ImmutablePicoContainer && ((ImmutablePicoContainer) obj).delegate == delegate)
-            ;
-    }
-
-    @Override
-	public int hashCode() {
-        return delegate.hashCode();
-    }
-
-    @Override
-	public String toString() {
-        return "[Immutable]:" + delegate.toString();
-    }
-
-    public Converters getConverters() {
-        if (delegate instanceof Converting) {
-            return ((Converting) delegate).getConverters();
-        }
-        return new ConvertsNothing();
-    }
+    return new ConvertsNothing();
+  }
 }
