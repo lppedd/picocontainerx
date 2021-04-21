fa3ccaa6baca0029312a998e335b81e59064faab
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 19:35:40 +0200

    refactor: cleanup ConstructorParameters

--- picocontainer/src/main/java/com/picocontainer/parameters/ConstructorParameters.java
+++ picocontainer/src/main/java/com/picocontainer/parameters/ConstructorParameters.java
@@ -4,6 +4,7 @@
 package com.picocontainer.parameters;
 
 import com.picocontainer.Parameter;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 
@@ -13,35 +14,30 @@ import java.util.Arrays;
  */
 @SuppressWarnings("serial")
 public class ConstructorParameters extends AccessibleObjectParameterSet {
+  /**
+   * Reference this in your constructor parameters if you wish the no-arg constructor to be used.
+   */
+  public static final ConstructorParameters NO_ARG_CONSTRUCTOR = new ConstructorParameters(new Parameter[0]);
 
-	/**
-	 * Reference this in your constructor parameters if you wish the no-arg constructor to be used.
-	 */
-	public static final ConstructorParameters NO_ARG_CONSTRUCTOR = new ConstructorParameters(new Parameter[0]);
+  /**
+   * Constructs constructor parameters with the given component and constant parameters.
+   *
+   * @param params the parameters in constructor parameter order
+   */
+  public ConstructorParameters(@Nullable final Parameter @Nullable [] params) {
+    super(null, params);
+  }
 
+  public ConstructorParameters(final Parameter parameter) {
+    super(null, parameter);
+  }
 
-	/**
-	 * Constructs constructor parameters with the given component and constant parameters.
-	 * @param params the parameters in constructor parmeter order.
-	 */
-	public ConstructorParameters(final Parameter[] params) {
-		super(null, params);
-	}
+  public ConstructorParameters() {
+    this((Parameter[]) null);
+  }
 
-
-	public ConstructorParameters(final Parameter parameter) {
-		super(null, new Parameter[] {parameter});
-	}
-
-	public ConstructorParameters() {
-		this((Parameter[])null);
-	}
-
-
-	@Override
-	public String toString() {
-		return "ConstructorParameters Parameters = " + Arrays.deepToString(this.getParams());
-	}
-
-
+  @Override
+  public String toString() {
+    return "ConstructorParameters Parameters = " + Arrays.deepToString(getParams());
+  }
 }
