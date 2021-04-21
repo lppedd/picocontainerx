878f196b5b5e0982131ff6b1afa7f200a99eb728
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 19:32:33 +0200

    refactor: cleanup FieldParameters

--- picocontainer/src/main/java/com/picocontainer/parameters/FieldParameters.java
+++ picocontainer/src/main/java/com/picocontainer/parameters/FieldParameters.java
@@ -2,14 +2,16 @@ package com.picocontainer.parameters;
 
 import com.picocontainer.Parameter;
 
+/**
+ * TODO
+ */
+@SuppressWarnings("serial")
 public class FieldParameters extends AccessibleObjectParameterSet {
+  public FieldParameters(final Class<?> targetType, final String name, final Parameter... params) {
+    super(targetType, name, params);
+  }
 
-	public FieldParameters(final Class<?> targetType, final String name, final Parameter... params) {
-		super(targetType, name, params);
-	}
-
-	public FieldParameters(final String name, final Parameter... params) {
-		super(name, params);
-	}
-
+  public FieldParameters(final String name, final Parameter... params) {
+    super(name, params);
+  }
 }
