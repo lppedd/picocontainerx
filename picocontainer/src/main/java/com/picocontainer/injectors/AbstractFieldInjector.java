9d6fa3d692e202f2654644cf0f70f2950c4b821d
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 11:56:30 +0200

    refactor: cleanup AbstractFieldInjector

--- picocontainer/src/main/java/com/picocontainer/injectors/AbstractFieldInjector.java
+++ picocontainer/src/main/java/com/picocontainer/injectors/AbstractFieldInjector.java
@@ -21,49 +21,56 @@ import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * TODO
+ */
 @SuppressWarnings("serial")
 public abstract class AbstractFieldInjector<T> extends IterativeInjector<T> {
+  public AbstractFieldInjector(
+      final Object componentKey,
+      final Class<?> componentImplementation,
+      final ComponentMonitor monitor,
+      final boolean useNames,
+      final boolean requireConsumptionOfAllParameters,
+      final FieldParameters... parameters) {
+    super(componentKey, componentImplementation, monitor, useNames, requireConsumptionOfAllParameters, parameters);
+  }
 
-	public AbstractFieldInjector(final Object componentKey, final Class<?> componentImplementation,
-			final ComponentMonitor monitor, final boolean useNames, final boolean requireConsumptionOfallParameters, final FieldParameters... parameters)
-			throws com.picocontainer.injectors.AbstractInjector.NotConcreteRegistrationException {
-		super(componentKey, componentImplementation, monitor, useNames, requireConsumptionOfallParameters, parameters);
-	}
+  @Override
+  protected final void unsatisfiedDependencies(
+      final PicoContainer container,
+      final Set<Type> unsatisfiableDependencyTypes,
+      final List<AccessibleObject> unsatisfiableDependencyMembers) {
+    final StringBuilder sb = new StringBuilder(getComponentImplementation().getName());
+    sb.append(" has unsatisfied dependency for fields [");
 
-	@Override
-	final protected void unsatisfiedDependencies(final PicoContainer container,
-			final Set<Type> unsatisfiableDependencyTypes, final List<AccessibleObject> unsatisfiableDependencyMembers) {
-		final StringBuilder sb = new StringBuilder(this.getComponentImplementation().getName())
-				.append(" has unsatisfied dependency for fields [");
-		for (int i = 0; i < unsatisfiableDependencyMembers.size(); i++) {
-			final AccessibleObject accessibleObject = unsatisfiableDependencyMembers.get(i);
-			final Field m = (Field) accessibleObject;
-			sb
-					.append(" ")
-					.append(m.getDeclaringClass().getName())
-					.append(".")
-					.append(m.getName())
-					.append(" (field's type is ")
-					.append(m.getType().getName())
-					.append(") ");
-		}
-		final String container1 = container.toString();
-		throw new com.picocontainer.injectors.AbstractInjector.UnsatisfiableDependenciesException(sb.toString() + "] from " + container1);
-	}
+    for (final AccessibleObject accessibleObject : unsatisfiableDependencyMembers) {
+      final Field m = (Field) accessibleObject;
+      sb.append(" ")
+          .append(m.getDeclaringClass().getName())
+          .append(".")
+          .append(m.getName())
+          .append(" (field's type is ")
+          .append(m.getType().getName())
+          .append(") ");
+    }
 
+    throw new UnsatisfiableDependenciesException(sb + "] from " + container.toString());
+  }
 
-	@Override
-	protected boolean isAccessibleObjectEqualToParameterTarget(final AccessibleObject testObject,
-			final Parameter currentParameter) {
-		if (currentParameter.getTargetName() == null) {
-			return false;
-		}
+  @Override
+  protected boolean isAccessibleObjectEqualToParameterTarget(
+      final AccessibleObject testObject,
+      final Parameter currentParameter) {
+    if (currentParameter.getTargetName() == null) {
+      return false;
+    }
 
-		if (!(testObject instanceof Field)) {
-			throw new PicoCompositionException(testObject + " must be a field to use setter injection");
-		}
+    if (!(testObject instanceof Field)) {
+      throw new PicoCompositionException(testObject + " must be a field to use setter injection");
+    }
 
-		Field testField = (Field)testObject;
-		return testField.getName().equals(currentParameter.getTargetName());
-	}
+    final Field testField = (Field) testObject;
+    return testField.getName().equals(currentParameter.getTargetName());
+  }
 }
