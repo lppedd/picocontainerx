2c539cf4f510b72676763a9d9e64128e1f57143e
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 10:56:09 +0200

    refactor: cleanup Synchronizing

--- picocontainer/src/main/java/com/picocontainer/behaviors/Synchronizing.java
+++ picocontainer/src/main/java/com/picocontainer/behaviors/Synchronizing.java
@@ -9,7 +9,11 @@
  *****************************************************************************/
 package com.picocontainer.behaviors;
 
-import com.picocontainer.*;
+import com.picocontainer.Characteristics;
+import com.picocontainer.ComponentAdapter;
+import com.picocontainer.ComponentMonitor;
+import com.picocontainer.LifecycleStrategy;
+import com.picocontainer.PicoContainer;
 import com.picocontainer.parameters.ConstructorParameters;
 import com.picocontainer.parameters.FieldParameters;
 import com.picocontainer.parameters.MethodParameters;
@@ -18,58 +22,89 @@ import java.lang.reflect.Type;
 import java.util.Properties;
 
 /**
- * This behavior factory provides <strong>synchronized</strong> wrappers to control access to a particular component.
- *  It is recommended that you use {@link com.picocontainer.behaviors.Locking} instead since it results in better performance
- *  and does the same job.
+ * This behavior factory provides {@code synchronized} wrappers to control access to a particular component.
+ * It is recommended that you use {@link Locking} instead since it results in better performance and does the same job.
+ *
  * @author Aslak Helles&oslash;y
  */
 @SuppressWarnings("serial")
 public class Synchronizing extends AbstractBehavior {
-
-    /** {@inheritDoc} **/
-	@Override
-	public <T> ComponentAdapter<T> createComponentAdapter(final ComponentMonitor monitor, final LifecycleStrategy lifecycle, final Properties componentProps,
-                                                 final Object key, final Class<T> impl, final ConstructorParameters constructorParams, final FieldParameters[] fieldParams, final MethodParameters[] methodParams) {
-       if (removePropertiesIfPresent(componentProps, Characteristics.NO_SYNCHRONIZE)) {
-    	   return super.createComponentAdapter(monitor, lifecycle, componentProps, key, impl, constructorParams, fieldParams, methodParams);
-       }
-
-    	removePropertiesIfPresent(componentProps, Characteristics.SYNCHRONIZE);
-        return monitor.changedBehavior(new Synchronized<T>(super.createComponentAdapter(monitor, lifecycle, componentProps, key, impl, constructorParams, fieldParams, methodParams)));
+  @Override
+  public <T> ComponentAdapter<T> createComponentAdapter(
+      final ComponentMonitor monitor,
+      final LifecycleStrategy lifecycle,
+      final Properties componentProps,
+      final Object key,
+      final Class<T> impl,
+      final ConstructorParameters constructorParams,
+      final FieldParameters[] fieldParams,
+      final MethodParameters[] methodParams) {
+    if (removePropertiesIfPresent(componentProps, Characteristics.NO_SYNCHRONIZE)) {
+      return super.createComponentAdapter(
+          monitor,
+          lifecycle,
+          componentProps,
+          key,
+          impl,
+          constructorParams,
+          fieldParams,
+          methodParams
+      );
     }
 
-    /** {@inheritDoc} **/
+    removePropertiesIfPresent(componentProps, Characteristics.SYNCHRONIZE);
+    return monitor.changedBehavior(
+        new Synchronized<>(
+            super.createComponentAdapter(
+                monitor,
+                lifecycle,
+                componentProps,
+                key,
+                impl,
+                constructorParams,
+                fieldParams,
+                methodParams
+            )
+        )
+    );
+  }
+
+  @Override
+  public <T> ComponentAdapter<T> addComponentAdapter(
+      final ComponentMonitor monitor,
+      final LifecycleStrategy lifecycle,
+      final Properties componentProps,
+      final ComponentAdapter<T> adapter) {
+    if (removePropertiesIfPresent(componentProps, Characteristics.NO_SYNCHRONIZE)) {
+      return super.addComponentAdapter(monitor, lifecycle, componentProps, adapter);
+    }
+
+    removePropertiesIfPresent(componentProps, Characteristics.SYNCHRONIZE);
+    return monitor.changedBehavior(
+        new Synchronized<>(super.addComponentAdapter(monitor, lifecycle, componentProps, adapter))
+    );
+  }
+
+  /**
+   * {@link ComponentAdapter} that uses {@code synchronized} around {@link ComponentAdapter#getComponentInstance}.
+   *
+   * @author Aslak Helles&oslash;y
+   * @author Manish Shah
+   */
+  @SuppressWarnings("serial")
+  public static class Synchronized<T> extends AbstractChangedBehavior<T> {
+    public Synchronized(final ComponentAdapter<T> delegate) {
+      super(delegate);
+    }
+
     @Override
-	public <T> ComponentAdapter<T> addComponentAdapter(final ComponentMonitor monitor, final LifecycleStrategy lifecycle, final Properties componentProps,
-                                                final ComponentAdapter<T> adapter) {
-        if (removePropertiesIfPresent(componentProps, Characteristics.NO_SYNCHRONIZE)) {
-        	return super.addComponentAdapter(monitor, lifecycle, componentProps, adapter);
-        }
-
-    	removePropertiesIfPresent(componentProps, Characteristics.SYNCHRONIZE);
-        return monitor.changedBehavior(new Synchronized<T>(super.addComponentAdapter(monitor, lifecycle, componentProps, adapter)));
+    public synchronized T getComponentInstance(final PicoContainer container, final Type into) {
+      return super.getComponentInstance(container, into);
     }
 
-    /**
-     * Component Adapter that uses java synchronized around getComponentInstance().
-     * @author Aslak Helles&oslash;y
-     * @author Manish Shah
-     */
-    @SuppressWarnings("serial")
-    public static class Synchronized<T> extends AbstractChangedBehavior<T> {
-
-        public Synchronized(final ComponentAdapter<T> delegate) {
-            super(delegate);
-        }
-
-        @Override
-		public synchronized T getComponentInstance(final PicoContainer container, final Type into) throws PicoCompositionException {
-            return super.getComponentInstance(container, into);
-        }
-
-        public String getDescriptor() {
-            return "Synchronized";
-        }
-
+    @Override
+    public String getDescriptor() {
+      return "Synchronized";
     }
+  }
 }
