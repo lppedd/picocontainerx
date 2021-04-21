8d5996d6bff6e02de384c3f6a6986dd96624dabf
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 19:27:01 +0200

    refactor: cleanup NullParameter

--- picocontainer/src/main/java/com/picocontainer/parameters/NullParameter.java
+++ picocontainer/src/main/java/com/picocontainer/parameters/NullParameter.java
@@ -7,85 +7,89 @@
  ******************************************************************************/
 package com.picocontainer.parameters;
 
-import com.picocontainer.*;
+import com.picocontainer.ComponentAdapter;
+import com.picocontainer.NameBinding;
+import com.picocontainer.PicoCompositionException;
+import com.picocontainer.PicoContainer;
+import com.picocontainer.PicoVisitor;
 
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
 /**
- * Once in a great while, you actually want to pass in 'null' as an argument.  Instead
- * of bypassing the type checking mechanisms available in
- * {@link com.picocontainer.parameters.ConstantParameter ConstantParameter}, we provide a  <em>Special Type</em>
- * geared to marking nulls.
- * @author Michael Rimov
+ * Once in a great while, you actually want to pass in 'null' as an argument.
+ * Instead of bypassing the type checking mechanisms available in {@link ConstantParameter},
+ * we provide a special type geared to marking nulls.
  *
+ * @author Michael Rimov
  */
 @SuppressWarnings("serial")
 public class NullParameter extends AbstractParameter implements Serializable {
+  public static final NullParameter INSTANCE = new NullParameter();
 
-	/**
-	 * The one and only instance of null parameter.
-	 */
-	public static final NullParameter INSTANCE = new NullParameter();
+  /**
+   * Only one instance of Null parameter needed.
+   */
+  private NullParameter() {}
 
-	/**
-	 * Only once instance of Null parameter needed.
-	 */
-	protected NullParameter() {
-	}
+  /**
+   * @see com.picocontainer.Parameter#accept(PicoVisitor)
+   */
+  @Override
+  public void accept(final PicoVisitor visitor) {
+    visitor.visitParameter(this);
+  }
 
-	/**
-	 * {@inheritDoc}
-	 * @see com.picocontainer.Parameter#accept(com.picocontainer.PicoVisitor)
-	 */
-	public void accept(final PicoVisitor visitor) {
-		visitor.visitParameter(this);
-	}
+  /**
+   * @see com.picocontainer.Parameter#resolve(PicoContainer, ComponentAdapter, ComponentAdapter, Type, NameBinding,
+   * boolean, Annotation)
+   */
+  @Override
+  public Resolver resolve(
+      final PicoContainer container,
+      final ComponentAdapter<?> forAdapter,
+      final ComponentAdapter<?> injecteeAdapter,
+      final Type expectedType,
+      final NameBinding expectedNameBinding,
+      final boolean useNames,
+      final Annotation binding) {
+    return new ValueResolver(isAssignable(expectedType), null, null);
+  }
 
-	/**
-	 * {@inheritDoc}
-	 * @see com.picocontainer.Parameter#resolve(com.picocontainer.PicoContainer, com.picocontainer.ComponentAdapter, com.picocontainer.ComponentAdapter, Type, com.picocontainer.NameBinding, boolean, Annotation)
-	 */
-	public Resolver resolve(final PicoContainer container,
-			final ComponentAdapter<?> forAdapter,
-			final ComponentAdapter<?> injecteeAdapter, final Type expectedType,
-			final NameBinding expectedNameBinding, final boolean useNames,
-			final Annotation binding) {
-		return new ValueResolver(isAssignable(expectedType), null, null);
-	}
+  /**
+   * @see com.picocontainer.Parameter#verify(PicoContainer, ComponentAdapter, Type,
+   * NameBinding, boolean, Annotation)
+   */
+  @Override
+  public void verify(
+      final PicoContainer container,
+      final ComponentAdapter<?> adapter,
+      final Type expectedType,
+      final NameBinding expectedNameBinding,
+      final boolean useNames,
+      final Annotation binding) {
+    if (!isAssignable(expectedType)) {
+      throw new PicoCompositionException(expectedType + " cannot be assigned a null value");
+    }
+  }
 
-	/**
-	 * {@inheritDoc}
-	 * @see com.picocontainer.Parameter#verify(com.picocontainer.PicoContainer, com.picocontainer.ComponentAdapter, Type, com.picocontainer.NameBinding, boolean, Annotation)
-	 */
-	public void verify(final PicoContainer container, final ComponentAdapter<?> adapter,
-			final Type expectedType, final NameBinding expectedNameBinding,
-			final boolean useNames, final Annotation binding) {
-		if (!isAssignable(expectedType)) {
-			throw new PicoCompositionException(expectedType + " cannot be assigned a null value");
-		}
-	}
-
-	/**
-	 * Nulls cannot be assigned to primitives.
-	 * @param expectedType
-	 * @return
-	 */
-    protected boolean isAssignable(final Type expectedType) {
-        if (expectedType instanceof Class<?>) {
-            Class<?> expectedClass = Class.class.cast(expectedType);
-            if (expectedClass.isPrimitive()) {
-                return false;
-            }
-        }
-        return true;
+  /**
+   * Nulls cannot be assigned to primitives.
+   *
+   * @param expectedType
+   */
+  protected boolean isAssignable(final Type expectedType) {
+    if (expectedType instanceof Class<?>) {
+      final Class<?> expectedClass = (Class<?>) expectedType;
+      return !expectedClass.isPrimitive();
     }
 
-	@Override
-	public String getTargetName() {
-		return null;
-	}
+    return true;
+  }
 
-
+  @Override
+  public String getTargetName() {
+    return null;
+  }
 }
