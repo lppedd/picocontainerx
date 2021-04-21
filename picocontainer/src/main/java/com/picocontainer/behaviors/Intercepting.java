0d40cacf91faf471a7900a1754a6fe4a71a845ad
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 10:47:07 +0200

    refactor: cleanup Intercepting

--- picocontainer/src/main/java/com/picocontainer/behaviors/Intercepting.java
+++ picocontainer/src/main/java/com/picocontainer/behaviors/Intercepting.java
@@ -8,10 +8,15 @@
  *****************************************************************************/
 package com.picocontainer.behaviors;
 
-import com.picocontainer.*;
+import com.picocontainer.ComponentAdapter;
+import com.picocontainer.ComponentMonitor;
+import com.picocontainer.LifecycleStrategy;
+import com.picocontainer.PicoContainer;
+import com.picocontainer.behaviors.ImplementationHiding.HiddenImplementation;
 import com.picocontainer.parameters.ConstructorParameters;
 import com.picocontainer.parameters.FieldParameters;
 import com.picocontainer.parameters.MethodParameters;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
@@ -25,194 +30,239 @@ import java.util.Properties;
  */
 @SuppressWarnings("serial")
 public class Intercepting extends AbstractBehavior {
+  @Override
+  public <T> ComponentAdapter<T> createComponentAdapter(
+      final ComponentMonitor monitor,
+      final LifecycleStrategy lifecycle,
+      final Properties componentProps,
+      final Object key,
+      final Class<T> impl,
+      final ConstructorParameters constructorParams,
+      final FieldParameters[] fieldParams,
+      final MethodParameters[] methodParams) {
+    return monitor.changedBehavior(
+        new Intercepted<>(
+            super.createComponentAdapter(
+                monitor,
+                lifecycle,
+                componentProps,
+                key,
+                impl,
+                constructorParams,
+                fieldParams,
+                methodParams
+            )
+        )
+    );
+  }
+
+  /**
+   * @author Paul Hammant
+   */
+  @SuppressWarnings("serial")
+  public static class Intercepted<T> extends HiddenImplementation<T> {
+    private final Map<Class<?>, Object> pres = new HashMap<>();
+    private final Map<Class<?>, Object> posts = new HashMap<>();
+    private final Controller controller = new ControllerWrapper(new InterceptorThreadLocal());
+
+    public Intercepted(final ComponentAdapter<T> delegate) {
+      super(delegate);
+    }
+
+    public void addPreInvocation(final Class<?> type, final Object interceptor) {
+      pres.put(type, interceptor);
+    }
+
+    public void addPostInvocation(final Class<?> type, final Object interceptor) {
+      posts.put(type, interceptor);
+    }
 
     @Override
-	public <T> ComponentAdapter<T> createComponentAdapter(final ComponentMonitor monitor,
-                                                          final LifecycleStrategy lifecycle,
-                                                          final Properties componentProps,
-                                                          final Object key,
-                                                          final Class<T> impl,
-                                                          final ConstructorParameters constructorParams, final FieldParameters[] fieldParams, final MethodParameters[] methodParams) throws PicoCompositionException {
-        return monitor.changedBehavior(new Intercepted<T>(super.createComponentAdapter(monitor,
-                lifecycle, componentProps, key,
-                impl, constructorParams, fieldParams, methodParams)));
+    protected Object invokeMethod(
+        final Object componentInstance,
+        final Method method,
+        final Object[] args,
+        final PicoContainer container) throws Throwable {
+      try {
+        controller.clear();
+        controller.instance(componentInstance);
+
+        final Object pre = pres.get(method.getDeclaringClass());
+
+        if (pre != null) {
+          final Object rv = method.invoke(pre, args);
+
+          if (controller.isVetoed()) {
+            return rv;
+          }
+        }
+
+        final Object result = method.invoke(componentInstance, args);
+        controller.setOriginalRetVal(result);
+        final Object post = posts.get(method.getDeclaringClass());
+
+        if (post != null) {
+          final Object rv = method.invoke(post, args);
+
+          if (controller.isOverridden()) {
+            return rv;
+          }
+        }
+
+        return result;
+      } catch (final InvocationTargetException ite) {
+        throw ite.getTargetException();
+      }
     }
 
-    /**
-     * @author Paul Hammant
-     */
-    @SuppressWarnings("serial")
-    public static class Intercepted<T> extends ImplementationHiding.HiddenImplementation<T> {
-
-        private final Map<Class, Object> pres = new HashMap<Class, Object>();
-        private final Map<Class, Object> posts = new HashMap<Class, Object>();
-        private final Controller controller = new ControllerWrapper(new InterceptorThreadLocal());
-
-        public Intercepted(final ComponentAdapter<T> delegate) {
-            super(delegate);
-        }
-
-        public void addPreInvocation(final Class type, final Object interceptor) {
-            pres.put(type, interceptor);
-        }
-
-        public void addPostInvocation(final Class type, final Object interceptor) {
-            posts.put(type, interceptor);
-        }
-
-        @Override
-        protected Object invokeMethod(final Object componentInstance, final Method method, final Object[] args, final PicoContainer container) throws Throwable {
-            try {
-                controller.clear();
-                controller.instance(componentInstance);
-                Object pre = pres.get(method.getDeclaringClass());
-                if (pre != null) {
-                    Object rv = method.invoke(pre, args);
-                    if (controller.isVetoed()) {
-                        return rv;
-                    }
-                }
-                Object result = method.invoke(componentInstance, args);
-                controller.setOriginalRetVal(result);
-                Object post = posts.get(method.getDeclaringClass());
-                if (post != null) {
-                    Object rv = method.invoke(post, args);
-                    if (controller.isOverridden()) {
-                        return rv;
-                    }
-                }
-                return result;
-            } catch (final InvocationTargetException ite) {
-                throw ite.getTargetException();
-            }
-        }
-
-        public Controller getController() {
-            return controller;
-        }
-
-        @Override
-		public String getDescriptor() {
-            return "Intercepted";
-        }
+    public Controller getController() {
+      return controller;
     }
 
-    public static class InterceptorThreadLocal extends ThreadLocal<Controller> implements Serializable {
+    @Override
+    public String getDescriptor() {
+      return "Intercepted";
+    }
+  }
 
-        @Override
-		protected Controller initialValue() {
-            return new ControllerImpl();
-        }
+  public static class InterceptorThreadLocal extends ThreadLocal<Controller> implements Serializable {
+    @Override
+    protected Controller initialValue() {
+      return new ControllerImpl();
+    }
+  }
+
+  public interface Controller {
+    void veto();
+
+    void clear();
+
+    boolean isVetoed();
+
+    void setOriginalRetVal(Object retVal);
+
+    boolean isOverridden();
+
+    void instance(Object instance);
+
+    Object getInstance();
+
+    Object getOriginalRetVal();
+
+    void override();
+  }
+
+  public static class ControllerImpl implements Controller {
+    private boolean vetoed;
+
+    @Nullable
+    private Object retVal;
+    private boolean overridden;
+
+    @Nullable
+    private Object instance;
+
+    @Override
+    public void veto() {
+      vetoed = true;
     }
 
-    public interface Controller {
-        void veto();
-
-        void clear();
-
-        boolean isVetoed();
-
-        void setOriginalRetVal(Object retVal);
-
-        boolean isOverridden();
-
-        void instance(Object instance);
-
-        Object getInstance();
-
-        Object getOriginalRetVal();
-
-        void override();
+    @Override
+    public void clear() {
+      vetoed = false;
+      overridden = false;
+      retVal = null;
+      instance = null;
     }
 
-    public static class ControllerImpl implements Controller {
-        private boolean vetoed;
-        private Object retVal;
-        private boolean overridden;
-        private Object instance;
-
-        public void veto() {
-            vetoed = true;
-        }
-
-        public void clear() {
-            vetoed = false;
-            overridden = false;
-            retVal = null;
-            instance = null;
-        }
-
-        public boolean isVetoed() {
-            return vetoed;
-        }
-
-        public void setOriginalRetVal(final Object retVal) {
-            this.retVal = retVal;
-        }
-
-        public Object getOriginalRetVal() {
-            return retVal;
-        }
-
-        public boolean isOverridden() {
-            return overridden;
-        }
-
-        public void instance(final Object instance) {
-            this.instance = instance;
-        }
-
-        public Object getInstance() {
-            return instance;
-        }
-
-        public void override() {
-            overridden = true;
-        }
+    @Override
+    public boolean isVetoed() {
+      return vetoed;
     }
 
-    public static class ControllerWrapper implements Controller {
-        private final ThreadLocal<Controller> threadLocal;
-
-        public ControllerWrapper(final ThreadLocal<Controller> threadLocal) {
-            this.threadLocal = threadLocal;
-        }
-
-        public void veto() {
-            threadLocal.get().veto();
-        }
-
-        public void clear() {
-            threadLocal.get().clear();
-        }
-
-        public boolean isVetoed() {
-            return threadLocal.get().isVetoed();
-        }
-
-        public void setOriginalRetVal(final Object retVal) {
-            threadLocal.get().setOriginalRetVal(retVal);
-        }
-
-        public Object getOriginalRetVal() {
-            return threadLocal.get().getOriginalRetVal();
-        }
-
-        public boolean isOverridden() {
-            return threadLocal.get().isOverridden();
-        }
-
-        public void instance(final Object instance) {
-            threadLocal.get().instance(instance);
-
-        }
-
-        public Object getInstance() {
-            return threadLocal.get().getInstance();
-        }
-
-        public void override() {
-            threadLocal.get().override();
-        }
+    @Override
+    public void setOriginalRetVal(final Object retVal) {
+      this.retVal = retVal;
     }
+
+    @Nullable
+    @Override
+    public Object getOriginalRetVal() {
+      return retVal;
+    }
+
+    @Override
+    public boolean isOverridden() {
+      return overridden;
+    }
+
+    @Override
+    public void instance(final Object instance) {
+      this.instance = instance;
+    }
+
+    @Nullable
+    @Override
+    public Object getInstance() {
+      return instance;
+    }
+
+    @Override
+    public void override() {
+      overridden = true;
+    }
+  }
+
+  public static class ControllerWrapper implements Controller {
+    private final ThreadLocal<? extends Controller> threadLocal;
+
+    public ControllerWrapper(final ThreadLocal<? extends Controller> threadLocal) {
+      this.threadLocal = threadLocal;
+    }
+
+    @Override
+    public void veto() {
+      threadLocal.get().veto();
+    }
+
+    @Override
+    public void clear() {
+      threadLocal.get().clear();
+    }
+
+    @Override
+    public boolean isVetoed() {
+      return threadLocal.get().isVetoed();
+    }
+
+    @Override
+    public void setOriginalRetVal(final Object retVal) {
+      threadLocal.get().setOriginalRetVal(retVal);
+    }
+
+    @Override
+    public Object getOriginalRetVal() {
+      return threadLocal.get().getOriginalRetVal();
+    }
+
+    @Override
+    public boolean isOverridden() {
+      return threadLocal.get().isOverridden();
+    }
+
+    @Override
+    public void instance(final Object instance) {
+      threadLocal.get().instance(instance);
+    }
+
+    @Override
+    public Object getInstance() {
+      return threadLocal.get().getInstance();
+    }
+
+    @Override
+    public void override() {
+      threadLocal.get().override();
+    }
+  }
 }
