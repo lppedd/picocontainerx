863694ee458260ee5cde6f544678c2e3cccce012
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 10:28:21 +0200

    refactor: cleanup Decorating

--- picocontainer/src/main/java/com/picocontainer/behaviors/Decorating.java
+++ picocontainer/src/main/java/com/picocontainer/behaviors/Decorating.java
@@ -10,7 +10,11 @@
 
 package com.picocontainer.behaviors;
 
-import com.picocontainer.*;
+import com.picocontainer.ComponentAdapter;
+import com.picocontainer.ComponentMonitor;
+import com.picocontainer.Decorator;
+import com.picocontainer.LifecycleStrategy;
+import com.picocontainer.PicoContainer;
 import com.picocontainer.parameters.ConstructorParameters;
 import com.picocontainer.parameters.FieldParameters;
 import com.picocontainer.parameters.MethodParameters;
@@ -19,42 +23,59 @@ import java.lang.reflect.Type;
 import java.util.Properties;
 
 /**
- * Behavior for Decorating. This factory will create {@link com.picocontainer.behaviors.Decorating.Decorated} that will
- * allow you to decorate what you like on the component instance that has been created
+ * Behavior for Decorating. This factory will create {@link Decorated} that will
+ * allow you to decorate what you like on the component instance that has been created.
  *
  * @author Paul Hammant
  */
+@SuppressWarnings("serial")
 public abstract class Decorating extends AbstractBehavior implements Decorator {
+  @Override
+  public <T> ComponentAdapter<T> createComponentAdapter(
+      final ComponentMonitor monitor,
+      final LifecycleStrategy lifecycle,
+      final Properties componentProps,
+      final Object key,
+      final Class<T> impl,
+      final ConstructorParameters constructorParams,
+      final FieldParameters[] fieldParams,
+      final MethodParameters[] methodParams) {
+    return monitor.changedBehavior(
+        new Decorated<>(
+            super.createComponentAdapter(
+                monitor,
+                lifecycle,
+                componentProps,
+                key,
+                impl,
+                constructorParams,
+                fieldParams,
+                methodParams
+            ),
+            this
+        )
+    );
+  }
+
+  @SuppressWarnings("serial")
+  public static class Decorated<T> extends AbstractChangedBehavior<T> {
+    private final Decorator decorator;
+
+    public Decorated(final ComponentAdapter<T> delegate, final Decorator decorator) {
+      super(delegate);
+      this.decorator = decorator;
+    }
 
     @Override
-	public <T> ComponentAdapter<T> createComponentAdapter(final ComponentMonitor monitor, final LifecycleStrategy lifecycle,
-                                                   final Properties componentProps, final Object key,
-                                                   final Class<T> impl, final ConstructorParameters constructorParams, final FieldParameters[] fieldParams, final MethodParameters[] methodParams) throws PicoCompositionException {
-        return monitor.changedBehavior(new Decorated<T>(super.createComponentAdapter(monitor, lifecycle,
-                componentProps,key, impl, constructorParams, fieldParams, methodParams), this));
+    public T getComponentInstance(final PicoContainer container, final Type into) {
+      final T instance = super.getComponentInstance(container, into);
+      decorator.decorate(instance);
+      return instance;
     }
 
-    @SuppressWarnings("serial")
-    public static class Decorated<T> extends AbstractChangedBehavior<T> {
-        private final Decorator decorator;
-
-        public Decorated(final ComponentAdapter<T> delegate, final Decorator decorator) {
-            super(delegate);
-            this.decorator = decorator;
-        }
-
-        @Override
-		public T getComponentInstance(final PicoContainer container, final Type into)
-                throws PicoCompositionException {
-            T instance = super.getComponentInstance(container, into);
-            decorator.decorate(instance);
-            return instance;
-        }
-
-        public String getDescriptor() {
-            return "Decorated";
-        }
-
+    @Override
+    public String getDescriptor() {
+      return "Decorated";
     }
-
+  }
 }
