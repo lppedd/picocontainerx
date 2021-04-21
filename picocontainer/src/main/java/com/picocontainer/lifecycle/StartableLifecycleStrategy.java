31f986b638051468ca9ae1785b89079d954d404b
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 12:51:50 +0200

    refactor: cleanup StartableLifecycleStrategy

--- picocontainer/src/main/java/com/picocontainer/lifecycle/StartableLifecycleStrategy.java
+++ picocontainer/src/main/java/com/picocontainer/lifecycle/StartableLifecycleStrategy.java
@@ -16,11 +16,14 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
- * Startable lifecycle strategy.  Starts and stops component if Startable,
- * and disposes it if Disposable.
- *
- * A subclass of this class can define other intrfaces for Startable/Disposable as well as other method names
- * for start/stop/dispose
+ * <p>
+ * Startable lifecycle strategy.
+ * Starts and stops component if {@link Startable}, and disposes it if {@link Disposable}.
+ * </p>
+ * <p>
+ * A subclass of this class can define other interfaces for {@link Startable}/{@link Disposable}
+ * as well as other method names for {@code start}/{@code stop}/{@code dispose}.
+ * </p>
  *
  * @author Mauro Talevi
  * @author J&ouml;rg Schaible
@@ -29,138 +32,146 @@ import java.lang.reflect.Method;
  */
 @SuppressWarnings("serial")
 public class StartableLifecycleStrategy extends AbstractMonitoringLifecycleStrategy {
+  private transient Method start;
+  private transient Method stop;
+  private transient Method dispose;
 
+  public StartableLifecycleStrategy(final ComponentMonitor monitor) {
+    super(monitor);
+  }
 
-	private transient Method start, stop, dispose;
+  private void doMethodsIfNotDone() {
+    try {
+      if (start == null) {
+        start = getStartableInterface().getMethod(getStartMethodName());
+      }
 
-    public StartableLifecycleStrategy(final ComponentMonitor monitor) {
-        super(monitor);
+      if (stop == null) {
+        stop = getStartableInterface().getMethod(getStopMethodName());
+      }
+
+      if (dispose == null) {
+        dispose = getDisposableInterface().getMethod(getDisposeMethodName());
+      }
+    } catch (final NoSuchMethodException ignored) {
+      //
     }
+  }
 
-    @SuppressWarnings("unchecked")
-	private void doMethodsIfNotDone() {
-        try {
-            if (start == null) {
-                start = getStartableInterface().getMethod(getStartMethodName());
-            }
-            if (stop == null) {
-                stop = getStartableInterface().getMethod(getStopMethodName());
-            }
-            if (dispose == null) {
-                dispose = getDisposableInterface().getMethod(getDisposeMethodName());
-            }
-        } catch (NoSuchMethodException e) {
-        }
-    }
+  /**
+   * Retrieve the lifecycle method name that represents the dispose method.
+   *
+   * @return the dispose method name. ('dispose')
+   */
+  protected String getDisposeMethodName() {
+    return "dispose";
+  }
 
-    /**
-     * Retrieve the lifecycle method name that represents the dispose method.
-     * @return the dispose method name. ('dispose')
-     */
-    protected String getDisposeMethodName() {
-        return "dispose";
-    }
+  /**
+   * Retrieve the lifecycle method name that represents the stop method.
+   *
+   * @return the stop method name ('stop')
+   */
+  protected String getStopMethodName() {
+    return "stop";
+  }
 
-    /**
-     * Retrieve the lifecycle method name that represents the stop method.
-     * @return the stop method name ('stop')
-     */
-    protected String getStopMethodName() {
-        return "stop";
-    }
+  /**
+   * Retrieve the lifecycle method name that represents the start method.
+   *
+   * @return the stop method name ('start')
+   */
+  protected String getStartMethodName() {
+    return "start";
+  }
 
-    /**
-     * Retrieve the lifecycle method name that represents the start method.
-     * @return the stop method name ('start')
-     */
-    protected String getStartMethodName() {
-        return "start";
-    }
+  @Override
+  public void start(final Object component) {
+    doMethodsIfNotDone();
 
+    if (component != null && getStartableInterface().isAssignableFrom(component.getClass())) {
+      final long str = System.currentTimeMillis();
+      currentMonitor().invoking(null, null, start, component);
 
-    /** {@inheritDoc} **/
-    @SuppressWarnings("unchecked")
-	public void start(final Object component) {
-        doMethodsIfNotDone();
-        if (component != null && getStartableInterface().isAssignableFrom(component.getClass())) {
-            long str = System.currentTimeMillis();
-            currentMonitor().invoking(null, null, start, component, new Object[0]);
-            try {
-                startComponent(component);
-                currentMonitor().invoked(null, null, start, component, System.currentTimeMillis() - str, null, new Object[0]);
-            } catch (RuntimeException cause) {
-                currentMonitor().lifecycleInvocationFailed(null, null, start, component, cause); // may re-throw
-            }
-        }
+      try {
+        startComponent(component);
+        currentMonitor().invoked(null, null, start, component, System.currentTimeMillis() - str, null);
+      } catch (final RuntimeException cause) {
+        currentMonitor().lifecycleInvocationFailed(null, null, start, component, cause); // may re-throw
+      }
     }
+  }
 
-    protected void startComponent(final Object component) {
-        doLifecycleMethod(component, start);
-    }
+  protected void startComponent(final Object component) {
+    doLifecycleMethod(component, start);
+  }
 
-    private void doLifecycleMethod(final Object component, final Method lifecycleMethod) {
-        try {
-            lifecycleMethod.invoke(component);
-        } catch (IllegalAccessException e) {
-            throw new PicoLifecycleException(lifecycleMethod, component, e);
-        } catch (InvocationTargetException e) {
-            if (e.getTargetException() instanceof RuntimeException) {
-                throw (RuntimeException) e.getTargetException();
-            }
-            throw new PicoLifecycleException(lifecycleMethod, component, e.getTargetException());
-        }
-    }
+  private void doLifecycleMethod(final Object component, final Method lifecycleMethod) {
+    try {
+      lifecycleMethod.invoke(component);
+    } catch (final IllegalAccessException e) {
+      throw new PicoLifecycleException(lifecycleMethod, component, e);
+    } catch (final InvocationTargetException e) {
+      if (e.getTargetException() instanceof RuntimeException) {
+        throw (RuntimeException) e.getTargetException();
+      }
 
-    protected void stopComponent(final Object component) {
-        doLifecycleMethod(component, stop);
+      throw new PicoLifecycleException(lifecycleMethod, component, e.getTargetException());
     }
-    protected void disposeComponent(final Object component) {
-        doLifecycleMethod(component, dispose);
-    }
+  }
 
-    /** {@inheritDoc} **/
-    @SuppressWarnings("unchecked")
-	public void stop(final Object component) {
-        doMethodsIfNotDone();
-        if (component != null && getStartableInterface().isAssignableFrom(component.getClass())) {
-            long str = System.currentTimeMillis();
-            currentMonitor().invoking(null, null, stop, component, new Object[0]);
-            try {
-                stopComponent(component);
-                currentMonitor().invoked(null, null, stop, component, System.currentTimeMillis() - str, null, new Object[0]);
-            } catch (RuntimeException cause) {
-                currentMonitor().lifecycleInvocationFailed(null, null, stop, component, cause); // may re-throw
-            }
-        }
-    }
+  protected void stopComponent(final Object component) {
+    doLifecycleMethod(component, stop);
+  }
 
-    /** {@inheritDoc} **/
-    @SuppressWarnings("unchecked")
-	public void dispose(final Object component) {
-        doMethodsIfNotDone();
-        if (component != null && getDisposableInterface().isAssignableFrom(component.getClass())) {
-            long str = System.currentTimeMillis();
-            currentMonitor().invoking(null, null, dispose, component, new Object[0]);
-            try {
-                disposeComponent(component);
-                currentMonitor().invoked(null, null, dispose, component, System.currentTimeMillis() - str, null, new Object[0]);
-            } catch (RuntimeException cause) {
-                currentMonitor().lifecycleInvocationFailed(null, null, dispose, component, cause); // may re-throw
-            }
-        }
-    }
+  protected void disposeComponent(final Object component) {
+    doLifecycleMethod(component, dispose);
+  }
 
-    /** {@inheritDoc} **/
-    @SuppressWarnings("unchecked")
-	public boolean hasLifecycle(final Class<?> type) {
-        return getStartableInterface().isAssignableFrom(type) || getDisposableInterface().isAssignableFrom(type);
-    }
+  @Override
+  public void stop(final Object component) {
+    doMethodsIfNotDone();
 
-    protected Class getDisposableInterface() {
-        return Disposable.class;
-    }
+    if (component != null && getStartableInterface().isAssignableFrom(component.getClass())) {
+      final long str = System.currentTimeMillis();
+      currentMonitor().invoking(null, null, stop, component);
 
-    protected Class getStartableInterface() {
-        return Startable.class;
+      try {
+        stopComponent(component);
+        currentMonitor().invoked(null, null, stop, component, System.currentTimeMillis() - str, null);
+      } catch (final RuntimeException cause) {
+        currentMonitor().lifecycleInvocationFailed(null, null, stop, component, cause); // may re-throw
+      }
     }
+  }
+
+  @Override
+  public void dispose(final Object component) {
+    doMethodsIfNotDone();
+
+    if (component != null && getDisposableInterface().isAssignableFrom(component.getClass())) {
+      final long str = System.currentTimeMillis();
+      currentMonitor().invoking(null, null, dispose, component);
+
+      try {
+        disposeComponent(component);
+        currentMonitor().invoked(null, null, dispose, component, System.currentTimeMillis() - str, null);
+      } catch (final RuntimeException cause) {
+        currentMonitor().lifecycleInvocationFailed(null, null, dispose, component, cause); // may re-throw
+      }
+    }
+  }
+
+  @Override
+  public boolean hasLifecycle(final Class<?> type) {
+    return getStartableInterface().isAssignableFrom(type) || getDisposableInterface().isAssignableFrom(type);
+  }
+
+  protected Class<?> getDisposableInterface() {
+    return Disposable.class;
+  }
+
+  protected Class<?> getStartableInterface() {
+    return Startable.class;
+  }
 }
