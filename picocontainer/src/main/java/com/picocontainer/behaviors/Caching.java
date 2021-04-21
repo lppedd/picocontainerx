0f4c60e05e9af8fe82117809835ff6dee6427334
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 10:27:08 +0200

    refactor: cleanup Caching

--- picocontainer/src/main/java/com/picocontainer/behaviors/Caching.java
+++ picocontainer/src/main/java/com/picocontainer/behaviors/Caching.java
@@ -10,7 +10,12 @@
 
 package com.picocontainer.behaviors;
 
-import com.picocontainer.*;
+import com.picocontainer.Characteristics;
+import com.picocontainer.ComponentAdapter;
+import com.picocontainer.ComponentMonitor;
+import com.picocontainer.LifecycleStrategy;
+import com.picocontainer.ObjectReference;
+import com.picocontainer.behaviors.Storing.Stored;
 import com.picocontainer.parameters.ConstructorParameters;
 import com.picocontainer.parameters.FieldParameters;
 import com.picocontainer.parameters.MethodParameters;
@@ -19,67 +24,89 @@ import com.picocontainer.references.SimpleReference;
 import java.util.Properties;
 
 /**
- * factory class creating cached behaviours
+ * Factory class creating cached behaviours
+ *
  * @author Aslak Helles&oslash;y
  * @author <a href="Rafal.Krzewski">rafal@caltha.pl</a>
  * @author Konstantin Pribluda
  */
 @SuppressWarnings("serial")
 public class Caching extends AbstractBehavior {
+  @Override
+  public <T> ComponentAdapter<T> createComponentAdapter(
+      final ComponentMonitor monitor,
+      final LifecycleStrategy lifecycle,
+      final Properties componentProps,
+      final Object key,
+      final Class<T> impl,
+      final ConstructorParameters constructorParams,
+      final FieldParameters[] fieldParams,
+      final MethodParameters[] methodParams) {
+    if (removePropertiesIfPresent(componentProps, Characteristics.NO_CACHE)) {
+      return super.createComponentAdapter(monitor, lifecycle, componentProps, key, impl, constructorParams, fieldParams, methodParams);
+    }
+
+    removePropertiesIfPresent(componentProps, Characteristics.CACHE);
+    return monitor.changedBehavior(
+        new Cached<>(
+            super.createComponentAdapter(
+                monitor,
+                lifecycle,
+                componentProps,
+                key,
+                impl,
+                constructorParams,
+                fieldParams,
+                methodParams
+            ),
+            new SimpleReference<>()
+        )
+    );
+  }
+
+  @Override
+  public <T> ComponentAdapter<T> addComponentAdapter(
+      final ComponentMonitor monitor,
+      final LifecycleStrategy lifecycle,
+      final Properties componentProps,
+      final ComponentAdapter<T> adapter) {
+    if (removePropertiesIfPresent(componentProps, Characteristics.NO_CACHE)) {
+      return super.addComponentAdapter(monitor, lifecycle, componentProps, adapter);
+    }
+
+    removePropertiesIfPresent(componentProps, Characteristics.CACHE);
+    final ComponentAdapter<T> delegate = super.addComponentAdapter(monitor, lifecycle, componentProps, adapter);
+    return monitor.changedBehavior(new Cached<>(delegate, new SimpleReference<>()));
+  }
+
+  /**
+   * <p>
+   * {@link ComponentAdapter} implementation that caches the component instance.
+   * </p>
+   * <p>
+   * This adapter supports components with a lifecycle, as it is a {@link com.picocontainer.ChangedBehavior},
+   * which will apply the delegate's {@link LifecycleStrategy} to the cached
+   * component instance.
+   * The lifecycle state is maintained so that the component instance behaves in the expected way:
+   * it can't be started if already started, it can't be started or stopped if disposed,
+   * it can't be stopped if not started, it can't be disposed if already disposed.
+   * </p>
+   *
+   * @author Mauro Talevi
+   */
+  @SuppressWarnings("serial")
+  public static class Cached<T> extends Stored<T> {
+    public Cached(final ComponentAdapter<T> delegate) {
+      this(delegate, new SimpleReference<>());
+    }
+
+    public Cached(final ComponentAdapter<T> delegate, final ObjectReference<Instance<T>> instanceReference) {
+      super(delegate, instanceReference);
+    }
 
     @Override
-	public <T> ComponentAdapter<T> createComponentAdapter(final ComponentMonitor monitor, final LifecycleStrategy lifecycle,
-			final Properties componentProps, final Object key, final Class<T> impl, final ConstructorParameters constructorParams, final FieldParameters[] fieldParams, final MethodParameters[] methodParams) throws PicoCompositionException {
-		if (removePropertiesIfPresent(componentProps, Characteristics.NO_CACHE)) {
-			return super.createComponentAdapter(monitor, lifecycle, componentProps, key, impl, constructorParams, fieldParams, methodParams);
-		}
-		removePropertiesIfPresent(componentProps, Characteristics.CACHE);
-        return monitor.changedBehavior(new Cached<T>(
-                super.createComponentAdapter(monitor, lifecycle, componentProps, key, impl, constructorParams, fieldParams, methodParams), new SimpleReference<Storing.Stored.Instance<T>>())
-       );
-	}
-
-	@Override
-	public <T> ComponentAdapter<T> addComponentAdapter(final ComponentMonitor monitor, final LifecycleStrategy lifecycle,
-			final Properties componentProps, final ComponentAdapter<T> adapter) {
-		if (removePropertiesIfPresent(componentProps, Characteristics.NO_CACHE)) {
-			return super.addComponentAdapter(monitor, lifecycle, componentProps, adapter);
-		}
-		removePropertiesIfPresent(componentProps, Characteristics.CACHE);
-        ComponentAdapter<T> delegate = super.addComponentAdapter(monitor, lifecycle, componentProps, adapter);
-        return monitor.changedBehavior(new Cached<T>(delegate, new SimpleReference<Storing.Stored.Instance<T>>()));
-	}
-
-    /**
-     * <p>
-     * {@link com.picocontainer.ComponentAdapter} implementation that caches the component instance.
-     * </p>
-     * <p>
-     * This adapter supports components with a lifecycle, as it is a
-     * {@link com.picocontainer.ChangedBehavior lifecycle manager} which will apply the delegate's
-     * {@link com.picocontainer.LifecycleStrategy lifecycle strategy} to the cached
-     * component instance. The lifecycle state is maintained so that the component
-     * instance behaves in the expected way: it can't be started if already started,
-     * it can't be started or stopped if disposed, it can't be stopped if not
-     * started, it can't be disposed if already disposed.
-     * </p>
-     *
-     * @author Mauro Talevi
-     */
-    @SuppressWarnings("serial")
-    public static class Cached<T> extends Storing.Stored<T> {
-
-        public Cached(final ComponentAdapter<T> delegate) {
-            this(delegate, new SimpleReference<Instance<T>>());
-        }
-
-        public Cached(final ComponentAdapter<T> delegate, final ObjectReference<Instance<T>> instanceReference) {
-            super(delegate, instanceReference);
-        }
-
-        @Override
-		public String getDescriptor() {
-            return "Cached" + getLifecycleDescriptor();
-        }
+    public String getDescriptor() {
+      return "Cached" + getLifecycleDescriptor();
     }
+  }
 }
