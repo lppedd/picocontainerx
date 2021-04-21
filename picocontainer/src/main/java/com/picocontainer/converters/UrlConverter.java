c27151dc357ab5415a8d1d6a3e77c82bfc5a8e0c
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 11:20:12 +0200

    refactor: cleanup UrlConverter

--- picocontainer/src/main/java/com/picocontainer/converters/UrlConverter.java
+++ picocontainer/src/main/java/com/picocontainer/converters/UrlConverter.java
@@ -6,18 +6,18 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
- * Converts values to URL data type objects
+ * Converts values to {@link URL} data type objects.
  *
  * @author Paul Hammant
  * @author Michael Rimov
  */
 public class UrlConverter implements Converter<URL> {
-
-    public URL convert(final String paramValue) {
-        try {
-            return new URL(paramValue);
-        } catch (MalformedURLException e) {
-            throw new PicoCompositionException(e);
-        }
+  @Override
+  public URL convert(final String paramValue) {
+    try {
+      return new URL(paramValue);
+    } catch (final MalformedURLException e) {
+      throw new PicoCompositionException(e);
     }
+  }
 }
