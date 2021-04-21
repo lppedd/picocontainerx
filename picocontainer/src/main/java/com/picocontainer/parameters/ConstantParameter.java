cc412e9ee1a4a7e32baf36bb35e3d8faf2f038a2
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 19:38:11 +0200

    refactor: cleanup ConstantParameter

--- picocontainer/src/main/java/com/picocontainer/parameters/ConstantParameter.java
+++ picocontainer/src/main/java/com/picocontainer/parameters/ConstantParameter.java
@@ -10,7 +10,11 @@
 
 package com.picocontainer.parameters;
 
-import com.picocontainer.*;
+import com.picocontainer.ComponentAdapter;
+import com.picocontainer.NameBinding;
+import com.picocontainer.PicoCompositionException;
+import com.picocontainer.PicoContainer;
+import com.picocontainer.PicoVisitor;
 
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
@@ -19,9 +23,9 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
 /**
- * A ConstantParameter should be used to pass in "constant" arguments to constructors. This
- * includes {@link String}s,{@link Integer}s or any other object that is not registered in
- * the container.
+ * Should be used to pass in "constant" arguments to constructors.
+ * This includes {@link String}s, {@link Integer}s or any other object
+ * that is not registered in the container.
  *
  * @author Jon Tirs&eacute;n
  * @author Aslak Helles&oslash;y
@@ -29,79 +33,77 @@ import java.lang.reflect.Type;
  * @author Thomas Heller
  */
 @SuppressWarnings("serial")
-public class ConstantParameter extends AbstractParameter implements Parameter, Serializable {
+public class ConstantParameter extends AbstractParameter implements Serializable {
+  private final Object value;
 
-    private final Object value;
+  public ConstantParameter(final Object value) {
+    this.value = value;
+  }
 
-
-    public ConstantParameter(final Object value) {
-		this.value = value;
-
+  @Override
+  public Resolver resolve(
+      final PicoContainer container,
+      final ComponentAdapter<?> forAdapter,
+      final ComponentAdapter<?> injecteeAdapter,
+      final Type expectedType,
+      final NameBinding expectedNameBinding,
+      final boolean useNames,
+      final Annotation binding) {
+    if (expectedType instanceof Class) {
+      return new ValueResolver(isAssignable(expectedType), value, null);
     }
 
-    public Resolver resolve(final PicoContainer container, final ComponentAdapter<?> forAdapter,
-                            final ComponentAdapter<?> injecteeAdapter, final Type expectedType, final NameBinding expectedNameBinding,
-                            final boolean useNames, final Annotation binding) {
-        if (expectedType instanceof Class) {
-            return new Parameter.ValueResolver(isAssignable(expectedType), value, null);
-        } else if (expectedType instanceof ParameterizedType) {
-        	return new Parameter.ValueResolver(isAssignable(((ParameterizedType)expectedType).getRawType()), value, null);
-        }
-        return new Parameter.ValueResolver(true, value, null);
+    if (expectedType instanceof ParameterizedType) {
+      return new ValueResolver(isAssignable(((ParameterizedType) expectedType).getRawType()), value, null);
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * @see Parameter#verify(com.picocontainer.PicoContainer,com.picocontainer.ComponentAdapter, Type,com.picocontainer.NameBinding,boolean, Annotation)
-     */
-    public void verify(final PicoContainer container, final ComponentAdapter<?> adapter,
-                       final Type expectedType, final NameBinding expectedNameBinding,
-                       final boolean useNames, final Annotation binding) throws PicoException {
-        if (!isAssignable(expectedType)) {
-            throw new PicoCompositionException(
-                expectedType + " is not assignable from " +
-                        (value != null ? value.getClass().getName() : "null"));
-        }
+    return new ValueResolver(true, value, null);
+  }
+
+  @Override
+  public void verify(
+      final PicoContainer container,
+      final ComponentAdapter<?> adapter,
+      final Type expectedType,
+      final NameBinding expectedNameBinding,
+      final boolean useNames,
+      final Annotation binding) {
+    if (!isAssignable(expectedType)) {
+      throw new PicoCompositionException(
+          expectedType + " is not assignable from " + (value != null ? value.getClass().getName() : "null")
+      );
+    }
+  }
+
+  protected boolean isAssignable(final Type expectedType) {
+    if (expectedType instanceof Class) {
+      final Class<?> expectedClass = (Class<?>) expectedType;
+      return checkPrimitive(expectedClass) || expectedClass.isInstance(value);
     }
 
-    protected boolean isAssignable(final Type expectedType) {
-        if (expectedType instanceof Class) {
-            Class<?> expectedClass = (Class<?>) expectedType;
-            if (checkPrimitive(expectedClass) || expectedClass.isInstance(value)) {
-                return true;
-            }
-        }
-        return false;
+    return false;
+  }
+
+  @Override
+  public void accept(final PicoVisitor visitor) {
+    visitor.visitParameter(this);
+  }
+
+  private boolean checkPrimitive(final Class<?> expectedType) {
+    try {
+      if (expectedType.isPrimitive()) {
+        final Field field = value.getClass().getField("TYPE");
+        final Class<?> type = (Class<?>) field.get(value);
+        return expectedType.isAssignableFrom(type);
+      }
+    } catch (final NoSuchFieldException | IllegalAccessException ignored) {
+      //
     }
 
-    /**
-     * Visit the current {@link Parameter}.
-     *
-     * @see com.picocontainer.Parameter#accept(com.picocontainer.PicoVisitor)
-     */
-    public void accept(final PicoVisitor visitor) {
-        visitor.visitParameter(this);
-    }
+    return false;
+  }
 
-    private boolean checkPrimitive(final Class<?> expectedType) {
-        try {
-            if (expectedType.isPrimitive()) {
-                final Field field = value.getClass().getField("TYPE");
-                final Class<?> type = (Class<?>) field.get(value);
-                return expectedType.isAssignableFrom(type);
-            }
-        } catch (NoSuchFieldException e) {
-            //ignore
-        } catch (IllegalAccessException e) {
-            //ignore
-        }
-        return false;
-    }
-
-
-	public Object getValue() {
-		return value;
-	}
-
+  public Object getValue() {
+    return value;
+  }
 }
