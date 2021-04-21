b70dbb55dadd3f3027e7283d42ab9891ca2c539b
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 11:19:46 +0200

    refactor: cleanup ShortConverter

--- picocontainer/src/main/java/com/picocontainer/converters/ShortConverter.java
+++ picocontainer/src/main/java/com/picocontainer/converters/ShortConverter.java
@@ -1,15 +1,14 @@
 package com.picocontainer.converters;
 
-
 /**
- * Converts values to 'short' data type objects
+ * Converts values to {@link Short} data type objects.
  *
  * @author Paul Hammant
  * @author Michael Rimov
  */
 class ShortConverter implements Converter<Short> {
-
-    public Short convert(final String paramValue) {
-        return Short.valueOf(paramValue);
-    }
+  @Override
+  public Short convert(final String paramValue) {
+    return Short.valueOf(paramValue);
+  }
 }
