68c97b87b6c006d84ebb6e3df657b627d11899ab
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 18:59:01 +0200

    refactor: cleanup AccessibleObjectParameterSet

--- picocontainer/src/main/java/com/picocontainer/parameters/AccessibleObjectParameterSet.java
+++ picocontainer/src/main/java/com/picocontainer/parameters/AccessibleObjectParameterSet.java
@@ -6,59 +6,59 @@ import java.io.Serializable;
 import java.util.Arrays;
 
 /***
- * Serialization Warning.  While using a specific target type is more of an edge case, it
- * isn't serializable and will have to be re-applied after deserialization.
- * @author Michael Rimov
+ * Serialization warning: while using a specific target type is more of an edge case,
+ * it isn't serializable and will have to be re-applied after deserialization.
  *
+ * @author Michael Rimov
  */
 @SuppressWarnings("serial")
 public class AccessibleObjectParameterSet implements Serializable {
+  public static final AccessibleObjectParameterSet[] EMPTY = {};
 
+  /**
+   * Can't be serialized
+   */
+  private transient Class<?> targetType;
+  private final Parameter[] params;
+  private final String name;
 
-	public static final AccessibleObjectParameterSet[] EMPTY = new AccessibleObjectParameterSet[]{};
+  public AccessibleObjectParameterSet(
+      final String name,
+      final Parameter... params) {
+    this.name = name;
+    this.params = params;
+  }
 
-	/**
-	 * Can't be serialized
-	 */
-	private transient Class<?> targetType;
+  public AccessibleObjectParameterSet(
+      final Class<?> targetType,
+      final String name,
+      final Parameter... params) {
+    this.targetType = targetType;
+    this.name = name;
+    this.params = params;
+  }
 
-	private final Parameter[] params;
+  public Class<?> getTargetType() {
+    return targetType;
+  }
 
-	private final String name;
+  public void setTargetType(final Class<?> targetType) {
+    this.targetType = targetType;
+  }
 
+  public Parameter[] getParams() {
+    return params;
+  }
 
-	public AccessibleObjectParameterSet(final String name, final Parameter... params) {
-		this.name = name;
-		this.params = params;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public AccessibleObjectParameterSet(final Class<?> targetType, final String name, final Parameter... params) {
-		this.targetType = targetType;
-		this.name = name;
-		this.params = params;
-	}
-
-	public Class<?> getTargetType() {
-		return targetType;
-	}
-
-	public void setTargetType(final Class<?> targetType) {
-		this.targetType = targetType;
-	}
-
-	public Parameter[] getParams() {
-		return params;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-
-	@Override
-	public String toString() {
-		return "AccessibleObjectParameterSet [targetType=" + targetType + ", params=" + Arrays.toString(params)
-				+ ", name=" + name + "]";
-	}
-
+  @Override
+  public String toString() {
+    return "AccessibleObjectParameterSet "
+        + "[targetType=" + targetType
+        + ", params=" + Arrays.toString(params)
+        + ", name=" + name + "]";
+  }
 }
