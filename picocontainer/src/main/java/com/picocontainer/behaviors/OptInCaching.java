af819712cf0730b90fc28687826ebeacb8760e9c
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 10:52:40 +0200

    refactor: cleanup OptInCaching

--- picocontainer/src/main/java/com/picocontainer/behaviors/OptInCaching.java
+++ picocontainer/src/main/java/com/picocontainer/behaviors/OptInCaching.java
@@ -10,7 +10,11 @@
 
 package com.picocontainer.behaviors;
 
-import com.picocontainer.*;
+import com.picocontainer.Characteristics;
+import com.picocontainer.ComponentAdapter;
+import com.picocontainer.ComponentMonitor;
+import com.picocontainer.LifecycleStrategy;
+import com.picocontainer.behaviors.Caching.Cached;
 import com.picocontainer.parameters.ConstructorParameters;
 import com.picocontainer.parameters.FieldParameters;
 import com.picocontainer.parameters.MethodParameters;
@@ -18,41 +22,75 @@ import com.picocontainer.parameters.MethodParameters;
 import java.util.Properties;
 
 /**
- * Behavior that turns off Caching behavior by default.
- * <p>Example:</p>
+ * Behavior that turns off {@link Caching} behavior by default.<br>
+ * Example:
  * <pre>
- * 		import com.picocontainer.*;
- * 		import static com.picocontainer.Characteristics.*;
+ * import com.picocontainer.*;
+ * import static com.picocontainer.Characteristics.*;
  *
- * 		MutablePicoContainer mpc = new PicoBuilder().withBehaviors(new OptInCaching()).build();
- * 		mpc.addComponent(Map.class, HashMap.class) //Multiple Instances, no Caching.
- * 		mpc.as(CACHE).addComponent(Set.class, HashSet.class) //Single Cached Instance.
+ * MutablePicoContainer mpc = new PicoBuilder().withBehaviors(new OptInCaching()).build();
+ * mpc.addComponent(Map.class, HashMap.class) //Multiple Instances, no Caching.
+ * mpc.as(CACHE).addComponent(Set.class, HashSet.class) //Single Cached Instance.
  * </pre>
+ *
  * @author Aslak Helles&oslash;y
  * @author <a href="Rafal.Krzewski">rafal@caltha.pl</a>
  */
 @SuppressWarnings("serial")
 public class OptInCaching extends AbstractBehavior {
-
-    @Override
-	public <T> ComponentAdapter<T> createComponentAdapter(final ComponentMonitor monitor, final LifecycleStrategy lifecycle, final Properties componentProps, final Object key,
-    			final Class<T> impl, final ConstructorParameters constructorParams, final FieldParameters[] fieldParams, final MethodParameters[] methodParams) throws PicoCompositionException {
-        if (removePropertiesIfPresent(componentProps, Characteristics.CACHE)) {
-            return monitor.changedBehavior(new Caching.Cached<T>(
-                    super.createComponentAdapter(monitor, lifecycle, componentProps, key, impl, constructorParams, fieldParams, methodParams)));
-        }
-        removePropertiesIfPresent(componentProps, Characteristics.NO_CACHE);
-        return super.createComponentAdapter(monitor, lifecycle, componentProps, key, impl, constructorParams, fieldParams, methodParams);
+  @Override
+  public <T> ComponentAdapter<T> createComponentAdapter(
+      final ComponentMonitor monitor,
+      final LifecycleStrategy lifecycle,
+      final Properties componentProps,
+      final Object key,
+      final Class<T> impl,
+      final ConstructorParameters constructorParams,
+      final FieldParameters[] fieldParams,
+      final MethodParameters[] methodParams) {
+    if (removePropertiesIfPresent(componentProps, Characteristics.CACHE)) {
+      return monitor.changedBehavior(
+          new Cached<>(
+              super.createComponentAdapter(
+                  monitor,
+                  lifecycle,
+                  componentProps,
+                  key,
+                  impl,
+                  constructorParams,
+                  fieldParams,
+                  methodParams
+              )
+          )
+      );
     }
 
+    removePropertiesIfPresent(componentProps, Characteristics.NO_CACHE);
+    return super.createComponentAdapter(
+        monitor,
+        lifecycle,
+        componentProps,
+        key,
+        impl,
+        constructorParams,
+        fieldParams,
+        methodParams
+    );
+  }
 
-    @Override
-	public <T> ComponentAdapter<T> addComponentAdapter(final ComponentMonitor monitor, final LifecycleStrategy lifecycle,
-                                                       final Properties componentProps, final ComponentAdapter<T> adapter) {
-        if (removePropertiesIfPresent(componentProps, Characteristics.CACHE)) {
-            return monitor.changedBehavior(new Caching.Cached<T>(super.addComponentAdapter(monitor, lifecycle, componentProps, adapter)));
-        }
-        removePropertiesIfPresent(componentProps, Characteristics.NO_CACHE);
-        return super.addComponentAdapter(monitor, lifecycle, componentProps, adapter);
+  @Override
+  public <T> ComponentAdapter<T> addComponentAdapter(
+      final ComponentMonitor monitor,
+      final LifecycleStrategy lifecycle,
+      final Properties componentProps,
+      final ComponentAdapter<T> adapter) {
+    if (removePropertiesIfPresent(componentProps, Characteristics.CACHE)) {
+      return monitor.changedBehavior(
+          new Cached<>(super.addComponentAdapter(monitor, lifecycle, componentProps, adapter))
+      );
     }
+
+    removePropertiesIfPresent(componentProps, Characteristics.NO_CACHE);
+    return super.addComponentAdapter(monitor, lifecycle, componentProps, adapter);
+  }
 }
