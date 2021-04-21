96ee80d43ff700c29e235d388d028b98d386aa99
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 17:43:02 +0200

    refactor: cleanup ThreadLocalMapObjectReference

--- picocontainer/src/main/java/com/picocontainer/references/ThreadLocalMapObjectReference.java
+++ picocontainer/src/main/java/com/picocontainer/references/ThreadLocalMapObjectReference.java
@@ -13,24 +13,28 @@ import com.picocontainer.ObjectReference;
 import java.util.Map;
 
 /**
- * Gets and sets references on a map stored in Thread Local
+ * Gets and sets references on a map stored in {@link ThreadLocal}.
+ *
  * @author Paul Hammant
  */
 public class ThreadLocalMapObjectReference<T> implements ObjectReference<T> {
-    private final ThreadLocal<Map<Object, T>> threadLocal;
-    private final Object key;
+  private final ThreadLocal<? extends Map<Object, T>> threadLocal;
+  private final Object key;
 
-    public ThreadLocalMapObjectReference(final ThreadLocal<Map<Object, T>> threadLocal, final Object key) {
-        this.threadLocal = threadLocal;
-        this.key = key;
-    }
+  public ThreadLocalMapObjectReference(
+      final ThreadLocal<? extends Map<Object, T>> threadLocal,
+      final Object key) {
+    this.threadLocal = threadLocal;
+    this.key = key;
+  }
 
-    public T get() {
-        return threadLocal.get().get(key) ;
-    }
+  @Override
+  public T get() {
+    return threadLocal.get().get(key);
+  }
 
-    public void set(final T item) {
-        threadLocal.get().put(key, item) ;
-
-    }
+  @Override
+  public void set(final T item) {
+    threadLocal.get().put(key, item);
+  }
 }
