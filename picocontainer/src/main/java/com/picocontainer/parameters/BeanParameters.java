4d080dec591ca5b37d60865bb0a0aacd32fea4b5
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 19:16:52 +0200

    refactor: cleanup BeanParameters

--- picocontainer/src/main/java/com/picocontainer/parameters/BeanParameters.java
+++ picocontainer/src/main/java/com/picocontainer/parameters/BeanParameters.java
@@ -5,29 +5,24 @@ import com.picocontainer.Parameter;
 /**
  * Parameter where the name is the bean property name, and this class automatically converts the name to
  * the setter method it is looking for.
- * @author Mike
  *
+ * @author Mike
  */
 @SuppressWarnings("serial")
 public class BeanParameters extends MethodParameters {
+  public BeanParameters(final String name, final Parameter... params) {
+    super(convertPropertyNameToSetterName(name), params);
+  }
 
-	public BeanParameters(final String name, final Parameter... params) {
-		super(convertPropertyNameToSetterName(name), params);
-	}
+  public BeanParameters(final Class<?> targetType, final String name, final Parameter... params) {
+    super(targetType, convertPropertyNameToSetterName(name), params);
+  }
 
+  private static String convertPropertyNameToSetterName(final String name) {
+    if (name == null) {
+      throw new NullPointerException("name");
+    }
 
-	public BeanParameters(final Class<?> targetType, final String name, final Parameter... params) {
-		super(targetType, convertPropertyNameToSetterName(name), params);
-	}
-
-	private static String convertPropertyNameToSetterName(final String name) {
-		if (name == null) {
-			throw new NullPointerException("name");
-		}
-
-
-		return "set" + Character.toUpperCase(name.charAt(0)) +
-				(name.length() > 1 ? name.substring(1) : "");
-	}
-
+    return "set" + Character.toUpperCase(name.charAt(0)) + (name.length() > 1 ? name.substring(1) : "");
+  }
 }
