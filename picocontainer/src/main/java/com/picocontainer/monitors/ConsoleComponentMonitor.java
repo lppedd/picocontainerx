4dfda30224355abb20c9377ed63c4452c803e2ba
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 13:22:04 +0200

    refactor: cleanup ConsoleComponentMonitor

--- picocontainer/src/main/java/com/picocontainer/monitors/ConsoleComponentMonitor.java
+++ picocontainer/src/main/java/com/picocontainer/monitors/ConsoleComponentMonitor.java
@@ -10,7 +10,12 @@
 
 package com.picocontainer.monitors;
 
-import com.picocontainer.*;
+import com.picocontainer.ChangedBehavior;
+import com.picocontainer.ComponentAdapter;
+import com.picocontainer.ComponentMonitor;
+import com.picocontainer.Injector;
+import com.picocontainer.MutablePicoContainer;
+import com.picocontainer.PicoContainer;
 
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -19,12 +24,11 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 
-import static com.picocontainer.monitors.ComponentMonitorHelper.*;
-
 /**
  * A {@link ComponentMonitor} which writes to a {@link OutputStream}.
  * This is typically used to write to a console.
- * (TODO  After serialization, the output printstream is null)
+ * <p>
+ * TODO: after serialization, the output PrintStream is null
  *
  * @author Paul Hammant
  * @author Aslak Helles&oslash;y
@@ -32,111 +36,184 @@ import static com.picocontainer.monitors.ComponentMonitorHelper.*;
  */
 @SuppressWarnings("serial")
 public class ConsoleComponentMonitor implements ComponentMonitor, Serializable {
+  /**
+   * The outgoing print stream.
+   */
+  private final transient PrintStream out;
 
-	/**
-	 * The outgoing print stream.
-	 */
-    private final transient PrintStream out;
+  /**
+   * Delegate component monitor (for component monitor chains).
+   */
+  private final ComponentMonitor delegate;
 
-    /**
-     * Delegate component monitor (for component monitor chains).
-     */
-    private final ComponentMonitor delegate;
+  /**
+   * Constructs a console component monitor that sends output to {@link System#out}.
+   */
+  public ConsoleComponentMonitor() {
+    // noinspection UseOfSystemOutOrSystemErr
+    this(System.out);
+  }
 
-    /**
-     * Constructs a console component monitor that sends output to <tt>System.out</tt>.
-     */
-    public ConsoleComponentMonitor() {
-        this(System.out);
-    }
+  /**
+   * Constructs a console component monitor that sends output to the specified output stream.
+   *
+   * @param out the designated output stream
+   * Options include System.out, Socket streams, File streams, etc
+   */
+  public ConsoleComponentMonitor(final OutputStream out) {
+    this(out, new NullComponentMonitor());
+  }
 
-    /**
-     * Constructs a console component monitor that sends output to the specified output stream.
-     *
-     * @param out  the designated output stream.  Options include System.out, Socket streams, File streams,
-     * etc.
-     */
-    public ConsoleComponentMonitor(final OutputStream out) {
-        this(out, new NullComponentMonitor());
-    }
+  /**
+   * Constructs a console component monitor chain that sends output to the specified output stream
+   * and then sends all events to the delegate component monitor.
+   *
+   * @param out the output stream of choice
+   * @param delegate the next monitor in the component monitor chain to receive event information
+   */
+  public ConsoleComponentMonitor(final OutputStream out, final ComponentMonitor delegate) {
+    this.out = new PrintStream(out);
+    this.delegate = delegate;
+  }
 
-    /**
-     * Constructs a console component monitor chain that sends output to the specified output stream
-     * and then sends all events to the delegate component monitor.
-     * @param out the output stream of choice.
-     * @param delegate the next monitor in the component monitor chain to receive event information.
-     */
-    public ConsoleComponentMonitor(final OutputStream out, final ComponentMonitor delegate) {
-        this.out = new PrintStream(out);
-        this.delegate = delegate;
-    }
+  @Override
+  public <T> Constructor<T> instantiating(
+      final PicoContainer container,
+      final ComponentAdapter<T> componentAdapter,
+      final Constructor<T> constructor) {
+    out.println(
+        ComponentMonitorHelper.format(
+            ComponentMonitorHelper.INSTANTIATING,
+            ComponentMonitorHelper.ctorToString(constructor)
+        )
+    );
 
-    public <T> Constructor<T> instantiating(final PicoContainer container, final ComponentAdapter<T> componentAdapter,
-                                     final Constructor<T> constructor) {
-        out.println(format(ComponentMonitorHelper.INSTANTIATING, ctorToString(constructor)));
-        return delegate.instantiating(container, componentAdapter, constructor);
-    }
+    return delegate.instantiating(container, componentAdapter, constructor);
+  }
 
-    public <T> void instantiated(final PicoContainer container, final ComponentAdapter<T> componentAdapter,
-                             final Constructor<T> constructor,
-                             final Object instantiated,
-                             final Object[] parameters,
-                             final long duration) {
-        out.println(format(ComponentMonitorHelper.INSTANTIATED, ctorToString(constructor), duration, instantiated.getClass().getName(), parmsToString(parameters)));
-        delegate.instantiated(container, componentAdapter, constructor, instantiated, parameters, duration);
-    }
+  @Override
+  public <T> void instantiated(
+      final PicoContainer container,
+      final ComponentAdapter<T> componentAdapter,
+      final Constructor<T> constructor,
+      final Object instantiated,
+      final Object[] parameters,
+      final long duration) {
+    out.println(
+        ComponentMonitorHelper.format(
+            ComponentMonitorHelper.INSTANTIATED,
+            ComponentMonitorHelper.ctorToString(constructor),
+            duration,
+            instantiated.getClass().getName(),
+            ComponentMonitorHelper.parmsToString(parameters)
+        )
+    );
 
-    public <T> void instantiationFailed(final PicoContainer container,
-                                    final ComponentAdapter<T> componentAdapter,
-                                    final Constructor<T> constructor,
-                                    final Exception cause) {
-        out.println(format(ComponentMonitorHelper.INSTANTIATION_FAILED, ctorToString(constructor), cause.getMessage()));
-        delegate.instantiationFailed(container, componentAdapter, constructor, cause);
-    }
+    delegate.instantiated(container, componentAdapter, constructor, instantiated, parameters, duration);
+  }
 
-    public Object invoking(final PicoContainer container,
-                           final ComponentAdapter<?> componentAdapter,
-                           final Member member,
-                           final Object instance, final Object... args) {
-        out.println(format(ComponentMonitorHelper.INVOKING, memberToString(member), instance));
-        return delegate.invoking(container, componentAdapter, member, instance, args);
-    }
+  @Override
+  public <T> void instantiationFailed(
+      final PicoContainer container,
+      final ComponentAdapter<T> componentAdapter,
+      final Constructor<T> constructor,
+      final Exception cause) {
+    out.println(
+        ComponentMonitorHelper.format(
+            ComponentMonitorHelper.INSTANTIATION_FAILED,
+            ComponentMonitorHelper.ctorToString(constructor),
+            cause.getMessage()
+        )
+    );
 
-    public void invoked(final PicoContainer container,
-                        final ComponentAdapter<?> componentAdapter,
-                        final Member member,
-                        final Object instance,
-                        final long duration,
-                        final Object retVal, final Object... args) {
-        out.println(format(ComponentMonitorHelper.INVOKED, memberToString(member), instance, duration));
-        delegate.invoked(container, componentAdapter, member, instance, duration, retVal, args);
-    }
+    delegate.instantiationFailed(container, componentAdapter, constructor, cause);
+  }
 
-    public void invocationFailed(final Member member, final Object instance, final Exception cause) {
-        out.println(format(ComponentMonitorHelper.INVOCATION_FAILED, memberToString(member), instance, cause.getMessage()));
-        delegate.invocationFailed(member, instance, cause);
-    }
+  @Override
+  public Object invoking(
+      final PicoContainer container,
+      final ComponentAdapter<?> componentAdapter,
+      final Member member,
+      final Object instance,
+      final Object... args) {
+    out.println(
+        ComponentMonitorHelper.format(
+            ComponentMonitorHelper.INVOKING,
+            ComponentMonitorHelper.memberToString(member),
+            instance
+        )
+    );
 
-    public void lifecycleInvocationFailed(final MutablePicoContainer container,
-                                          final ComponentAdapter<?> componentAdapter, final Method method,
-                                          final Object instance,
-                                          final RuntimeException cause) {
-        out.println(format(ComponentMonitorHelper.LIFECYCLE_INVOCATION_FAILED, memberToString(method), instance, cause.getMessage()));
-        delegate.lifecycleInvocationFailed(container, componentAdapter, method, instance, cause);
-    }
+    return delegate.invoking(container, componentAdapter, member, instance, args);
+  }
 
-    public Object noComponentFound(final MutablePicoContainer container, final Object key) {
-        out.println(format(ComponentMonitorHelper.NO_COMPONENT, key));
-        return delegate.noComponentFound(container, key);
-    }
+  @Override
+  public void invoked(
+      final PicoContainer container,
+      final ComponentAdapter<?> componentAdapter,
+      final Member member,
+      final Object instance,
+      final long duration,
+      final Object retVal,
+      final Object... args) {
+    out.println(
+        ComponentMonitorHelper.format(
+            ComponentMonitorHelper.INVOKED,
+            ComponentMonitorHelper.memberToString(member),
+            instance,
+            duration
+        )
+    );
 
-    public Injector newInjector(final Injector injector) {
-        return delegate.newInjector(injector);
-    }
+    delegate.invoked(container, componentAdapter, member, instance, duration, retVal, args);
+  }
 
-    /** {@inheritDoc} **/
-    public ChangedBehavior changedBehavior(final ChangedBehavior changedBehavior) {
-        return delegate.changedBehavior(changedBehavior);
-    }
+  @Override
+  public void invocationFailed(final Member member, final Object instance, final Exception cause) {
+    out.println(
+        ComponentMonitorHelper.format(
+            ComponentMonitorHelper.INVOCATION_FAILED,
+            ComponentMonitorHelper.memberToString(member),
+            instance,
+            cause.getMessage()
+        )
+    );
 
+    delegate.invocationFailed(member, instance, cause);
+  }
+
+  @Override
+  public void lifecycleInvocationFailed(
+      final MutablePicoContainer container,
+      final ComponentAdapter<?> componentAdapter,
+      final Method method,
+      final Object instance,
+      final RuntimeException cause) {
+    out.println(
+        ComponentMonitorHelper.format(
+            ComponentMonitorHelper.LIFECYCLE_INVOCATION_FAILED,
+            ComponentMonitorHelper.memberToString(method),
+            instance,
+            cause.getMessage()
+        )
+    );
+
+    delegate.lifecycleInvocationFailed(container, componentAdapter, method, instance, cause);
+  }
+
+  @Override
+  public Object noComponentFound(final MutablePicoContainer container, final Object key) {
+    out.println(ComponentMonitorHelper.format(ComponentMonitorHelper.NO_COMPONENT, key));
+    return delegate.noComponentFound(container, key);
+  }
+
+  @Override
+  public <T> Injector<T> newInjector(final Injector<T> injector) {
+    return delegate.newInjector(injector);
+  }
+
+  @Override
+  public <T> ChangedBehavior<T> changedBehavior(final ChangedBehavior<T> changedBehavior) {
+    return delegate.changedBehavior(changedBehavior);
+  }
 }
