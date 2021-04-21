2b0244099b06fe118265fdc2aa856ec5777b0404
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 19:27:41 +0200

    refactor: cleanup MethodParameters

--- picocontainer/src/main/java/com/picocontainer/parameters/MethodParameters.java
+++ picocontainer/src/main/java/com/picocontainer/parameters/MethodParameters.java
@@ -2,21 +2,28 @@ package com.picocontainer.parameters;
 
 import com.picocontainer.Parameter;
 
+/**
+ * TODO
+ */
 @SuppressWarnings("serial")
 public class MethodParameters extends AccessibleObjectParameterSet {
+  public MethodParameters(
+      final String name,
+      final Parameter... params) {
+    super(name, params);
+  }
 
-	public MethodParameters(final String name, final Parameter... params) {
-		super(name, params);
-	}
-
-	/**
-	 * Allows you to specify a specific type that this parameter binds to, for example, allows
-	 * @param targetType
-	 * @param name
-	 * @param params
-	 */
-	public MethodParameters(final Class<?> targetType, final String name, final Parameter... params) {
-		super(targetType, name, params);
-	}
-
+  /**
+   * Allows you to specify a specific type that this parameter binds to, for example, allows
+   *
+   * @param targetType
+   * @param name
+   * @param params
+   */
+  public MethodParameters(
+      final Class<?> targetType,
+      final String name,
+      final Parameter... params) {
+    super(targetType, name, params);
+  }
 }
