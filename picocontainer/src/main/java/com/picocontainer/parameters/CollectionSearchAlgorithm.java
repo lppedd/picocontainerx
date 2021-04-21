91da831a33dfe8aa190402275c05eb6f24ed8b38
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 19:23:28 +0200

    refactor: cleanup CollectionSearchAlgorithm

--- picocontainer/src/main/java/com/picocontainer/parameters/CollectionSearchAlgorithm.java
+++ picocontainer/src/main/java/com/picocontainer/parameters/CollectionSearchAlgorithm.java
@@ -6,9 +6,15 @@ import com.picocontainer.PicoContainer;
 
 import java.util.Map;
 
+/**
+ * TODO
+ */
+@SuppressWarnings("InterfaceMayBeAnnotatedFunctional")
 public interface CollectionSearchAlgorithm {
-
-	Map<Object, ComponentAdapter<?>> getMatchingComponentAdapters(PicoContainer container, ComponentAdapter adapter,
-                                 Class keyType, Generic<?> valueType);
-
+  Map<Object, ComponentAdapter<?>> getMatchingComponentAdapters(
+      final PicoContainer container,
+      final ComponentAdapter<?> adapter,
+      final Class<?> keyType,
+      final Generic<?> valueType
+  );
 }
