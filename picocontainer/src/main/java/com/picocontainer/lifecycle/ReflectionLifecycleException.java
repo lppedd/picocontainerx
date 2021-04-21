f2867753bea0c8f7b246090a45a64799caf32e14
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 12:42:33 +0200

    refactor: cleanup ReflectionLifecycleException

--- picocontainer/src/main/java/com/picocontainer/lifecycle/ReflectionLifecycleException.java
+++ picocontainer/src/main/java/com/picocontainer/lifecycle/ReflectionLifecycleException.java
@@ -11,23 +11,14 @@ import com.picocontainer.PicoException;
 
 /**
  * Subclass of {@link PicoException} that is thrown when there is a problem
- * invoking lifecycle methods via reflection.
+ * invoking lifecycle methods via Reflection.
  *
  * @author Paul Hammant
  * @author Mauro Talevi
  */
 @SuppressWarnings("serial")
 public class ReflectionLifecycleException extends PicoException {
-
-
-	/**
-     * Construct a new exception with the specified cause and the specified detail message.
-     *
-     * @param message the message detailing the exception.
-     * @param cause   the exception that caused this one.
-     */
-    protected ReflectionLifecycleException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-
+  protected ReflectionLifecycleException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
 }
