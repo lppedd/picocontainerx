622bbe3e4c21743143ad1c56137902f281c7ec2d
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 13:16:40 +0200

    refactor: cleanup ComponentMonitorHelper

--- picocontainer/src/main/java/com/picocontainer/monitors/ComponentMonitorHelper.java
+++ picocontainer/src/main/java/com/picocontainer/monitors/ComponentMonitorHelper.java
@@ -23,106 +23,115 @@ import java.text.MessageFormat;
  *
  * @author Mauro Talevi
  */
-public final class ComponentMonitorHelper  {
+public final class ComponentMonitorHelper {
+  public static final String INSTANTIATING = "PicoContainer: instantiating {0}";
+  public static final String INSTANTIATED = "PicoContainer: instantiated {0} [{1} ms], component {2}, injected [{3}]";
+  public static final String INSTANTIATION_FAILED = "PicoContainer: instantiation failed: {0}, reason: {1}";
+  public static final String INVOKING = "PicoContainer: invoking {0} on {1}";
+  public static final String INVOKED = "PicoContainer: invoked {0} on {1} [{2} ms]";
+  public static final String INVOCATION_FAILED = "PicoContainer: invocation failed: {0} on {1}, reason: {2}";
+  public static final String LIFECYCLE_INVOCATION_FAILED = "PicoContainer: lifecycle invocation failed: {0} on {1}, reason: {2}";
+  public static final String NO_COMPONENT = "PicoContainer: No component for key: {0}";
 
-    public final static String INSTANTIATING = "PicoContainer: instantiating {0}";
-    public final static String INSTANTIATED = "PicoContainer: instantiated {0} [{1} ms], component {2}, injected [{3}]";
-    public final static String INSTANTIATION_FAILED = "PicoContainer: instantiation failed: {0}, reason: {1}";
-    public final static String INVOKING = "PicoContainer: invoking {0} on {1}";
-    public final static String INVOKED = "PicoContainer: invoked {0} on {1} [{2} ms]";
-    public final static String INVOCATION_FAILED = "PicoContainer: invocation failed: {0} on {1}, reason: {2}";
-    public final static String LIFECYCLE_INVOCATION_FAILED = "PicoContainer: lifecycle invocation failed: {0} on {1}, reason: {2}";
-    public final static String NO_COMPONENT = "PicoContainer: No component for key: {0}";
+  public static String format(final String template, final Object... arguments) {
+    return MessageFormat.format(template, arguments);
+  }
 
-    public static String format(final String template, final Object... arguments) {
-        return MessageFormat.format(template, arguments);
+  public static String parmsToString(final Object[] injected) {
+    if (injected == null) {
+      return "";
     }
 
-    public static String parmsToString(final Object[] injected) {
-    	if (injected == null) {
-    		return "";
-    	}
+    final StringBuilder sb = new StringBuilder();
 
-    	StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < injected.length; i++) {
-            String s = injected[i].getClass().getName();
-            sb.append(s);
-            if (i < injected.length-1) {
-                sb.append(", ");
-            }
+    for (int i = 0; i < injected.length; i++) {
+      final String s = injected[i].getClass().getName();
+      sb.append(s);
+
+      if (i < injected.length - 1) {
+        sb.append(", ");
+      }
+    }
+
+    return sb.toString();
+  }
+
+  public static String ctorToString(final Constructor<?> constructor) {
+    if (constructor == null) {
+      return "null";
+    }
+
+    final Class<?>[] params = constructor.getParameterTypes();
+    final StringBuilder sb = new StringBuilder(constructor.getName());
+    sb.append("(");
+
+    for (int i = 0; i < params.length; i++) {
+      final String s = params[i].getName();
+      sb.append(s);
+
+      if (i < params.length - 1) {
+        sb.append(", ");
+      }
+    }
+
+    sb.append(")");
+    return sb.toString();
+  }
+
+  public static String methodToString(final Member member) {
+    if (member == null) {
+      return "null";
+    }
+
+    final StringBuilder sb = new StringBuilder(member.getName());
+
+    if (member instanceof Method) {
+      final Class<?>[] params = ((Method) member).getParameterTypes();
+      sb.append("(");
+
+      for (int i = 0; i < params.length; i++) {
+        final String s = params[i].getName();
+        sb.append(s);
+
+        if (i < params.length - 1) {
+          sb.append(", ");
         }
-        return sb.toString();
+      }
+
+      sb.append(")");
     }
 
-    public static String ctorToString(final Constructor constructor) {
-    	if (constructor == null) {
-    		return "null";
-    	}
-    	
-        Class[] params = constructor.getParameterTypes();
-        StringBuffer sb = new StringBuffer(constructor.getName());
-        sb.append("(");
-        for (int i = 0; i < params.length; i++) {
-            String s = params[i].getName();
-            sb.append(s);
-            if (i < params.length-1) {
-                sb.append(", ");
-            }
-        }
-        sb.append(")");
-        return sb.toString();
+    return sb.toString();
+  }
+
+  public static String getDeclaringTypeString(final Member member) {
+    // noinspection SimplifiableIfStatement
+    if (member == null) {
+      return " null ";
     }
 
-    public static String methodToString(final Member member) {
-    	if (member == null) {
-    		return "null";
-    	}
-    	
-    	StringBuilder sb = new StringBuilder(member.getName());
+    return member.getDeclaringClass().getName();
+  }
 
-        if (member instanceof Method) {
-            Class<?>[] params = ((Method) member).getParameterTypes();
-            sb.append("(");
-            for (int i = 0; i < params.length; i++) {
-                String s = params[i].getName();
-                sb.append(s);
-                if (i < params.length-1) {
-                    sb.append(", ");
-                }
-            }
-            sb.append(")");
-        }
-        return sb.toString();
+  public static String memberToString(final Member member) {
+    if (member == null) {
+      return "null";
     }
 
-    public static String getDeclaringTypeString(final Member m) {
-    	if (m == null) {
-    		return " null ";
-    	}
-
-
-    	return m.getDeclaringClass().getName();
+    // noinspection SimplifiableIfStatement
+    if (member instanceof Field) {
+      return getDeclaringTypeString(member) + "." + toString((Field) member);
     }
 
-    public static String memberToString(final Member m) {
-    	if (m == null) {
-    		return "null";
-    	}
-    	
-        if (m instanceof Field) {
-            return getDeclaringTypeString(m) + "." +  toString((Field) m);
-        } else {
-            return getDeclaringTypeString(m) + "." +  methodToString(m);
-        }
+    return getDeclaringTypeString(member) + "." + methodToString(member);
+  }
+
+  public static String toString(final Field field) {
+    // noinspection SimplifiableIfStatement
+    if (field == null) {
+      return "null";
     }
 
-    public static String toString(final Field field) {
-    	if (field == null) {
-    		return "null";
-    	}
-        StringBuffer sb = new StringBuffer(field.getName());
-        sb.append("(").append(field.getName()).append(")");
-        return sb.toString();
-    }
-
+    return field.getName() + "(" + field.getName() + ")";
+  }
 }
