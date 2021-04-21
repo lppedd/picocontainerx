9367185d6b26773e23eab7ff3a376cfa9226351a
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-21 17:50:00 +0200

    refactor: cleanup MethodCallingVisitor

--- picocontainer/src/main/java/com/picocontainer/visitors/MethodCallingVisitor.java
+++ picocontainer/src/main/java/com/picocontainer/visitors/MethodCallingVisitor.java
@@ -18,103 +18,111 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * A PicoVisitor implementation, that calls methods on the components of a specific type.
+ * A {@link com.picocontainer.PicoVisitor} implementation that calls methods on the components of a specific type.
  *
  * @author Aslak Helles&oslash;y
  * @author J&ouml;rg Schaible
  */
 @SuppressWarnings("serial")
 public class MethodCallingVisitor extends TraversalCheckingVisitor implements Serializable {
+  // TODO: we must serialize method with read/writeObject ... and are our parent serializable ???
+  private final transient Method method;
+  private final Object[] arguments;
+  private final Class<?> type;
+  private final boolean visitInInstantiationOrder;
+  private final List<Object> componentInstances;
 
-    // TODO: we must serialize method with read/writeObject ... and are our parent serializable ???
-    private transient Method method;
-    private final Object[] arguments;
-    private final Class<?> type;
-    private final boolean visitInInstantiationOrder;
-    private final List componentInstances;
-
-    /**
-     * Construct a MethodCallingVisitor for a method with arguments.
-     *
-     * @param method the {@link Method} to invoke
-     * @param ofType the type of the components, that will be invoked
-     * @param visitInInstantiationOrder <code>true</code> if components are visited in instantiation order
-     * @param arguments the arguments for the method invocation (may be <code>null</code>)
-     * @throws NullPointerException if <tt>method</tt>, or <tt>ofType</tt> is <code>null</code>
-     */
-    public MethodCallingVisitor(final Method method, final Class<?> ofType, final Object[] arguments, final boolean visitInInstantiationOrder) {
-        if (method == null) {
-            throw new NullPointerException();
-        }
-        this.method = method;
-        this.arguments = arguments;
-        this.type = ofType;
-        this.visitInInstantiationOrder = visitInInstantiationOrder;
-        this.componentInstances = new ArrayList();
+  /**
+   * Construct a {@code MethodCallingVisitor} for a method with arguments.
+   *
+   * @param method the {@link Method} to invoke
+   * @param ofType the type of the components that will be invoked
+   * @param visitInInstantiationOrder {@code true} if components are visited in instantiation order
+   * @param arguments the arguments for the method invocation (may be {@code null})
+   */
+  public MethodCallingVisitor(
+      final Method method,
+      final Class<?> ofType,
+      final Object[] arguments,
+      final boolean visitInInstantiationOrder) {
+    if (method == null) {
+      throw new NullPointerException();
     }
 
-    /**
-     * Construct a MethodCallingVisitor for standard methods visiting the component in instantiation order.
-     *
-     * @param method the method to invoke
-     * @param ofType the type of the components, that will be invoked
-     * @param arguments the arguments for the method invocation (may be <code>null</code>)
-     * @throws NullPointerException if <tt>method</tt>, or <tt>ofType</tt> is <code>null</code>
-     */
-    public MethodCallingVisitor(final Method method, final Class ofType, final Object[] arguments) {
-        this(method, ofType, arguments, true);
+    this.method = method;
+    this.arguments = arguments;
+    type = ofType;
+    this.visitInInstantiationOrder = visitInInstantiationOrder;
+    componentInstances = new ArrayList<>();
+  }
+
+  /**
+   * Construct a {@code MethodCallingVisitor} for methods visiting the components in instantiation order.
+   *
+   * @param method the {@link Method} to invoke
+   * @param ofType the type of the components that will be invoked
+   * @param arguments the arguments for the method invocation (may be {@code null})
+   */
+  public MethodCallingVisitor(final Method method, final Class<?> ofType, final Object[] arguments) {
+    this(method, ofType, arguments, true);
+  }
+
+  @Override
+  public Object traverse(final Object node) {
+    componentInstances.clear();
+
+    try {
+      super.traverse(node);
+
+      if (!visitInInstantiationOrder) {
+        Collections.reverse(componentInstances);
+      }
+
+      for (final Object componentInstance : componentInstances) {
+        invoke(componentInstance);
+      }
+    } finally {
+      componentInstances.clear();
     }
 
-    @Override
-	public Object traverse(final Object node) {
-        componentInstances.clear();
-        try {
-            super.traverse(node);
-            if (!visitInInstantiationOrder) {
-                Collections.reverse(componentInstances);
-            }
-            for (Object componentInstance : componentInstances) {
-                invoke(componentInstance);
-            }
-        } finally {
-            componentInstances.clear();
-        }
-        return Void.TYPE;
+    return Void.TYPE;
+  }
+
+  @Override
+  public boolean visitContainer(final PicoContainer pico) {
+    super.visitContainer(pico);
+    componentInstances.addAll(pico.getComponents(type));
+    return CONTINUE_TRAVERSAL;
+  }
+
+  protected Method getMethod() {
+    return method;
+  }
+
+  protected Object[] getArguments() {
+    return arguments;
+  }
+
+  protected void invoke(final Object[] targets) {
+    for (final Object target : targets) {
+      invoke(target);
+    }
+  }
+
+  protected Class<Void> invoke(final Object target) {
+    final Method method = getMethod();
+
+    try {
+      method.invoke(target, getArguments());
+    } catch (final IllegalArgumentException | IllegalAccessException e) {
+      throw new PicoCompositionException("Can't call " + method.getName() + " on " + target, e);
+    } catch (final InvocationTargetException e) {
+      throw new PicoCompositionException(
+          "Failed when calling " + method.getName() + " on " + target,
+          e.getTargetException()
+      );
     }
 
-    @Override
-	public boolean visitContainer(final PicoContainer pico) {
-        super.visitContainer(pico);
-        componentInstances.addAll(pico.getComponents(type));
-        return CONTINUE_TRAVERSAL;
-    }
-
-    protected Method getMethod() {
-        return method;
-    }
-
-    protected Object[] getArguments() {
-        return arguments;
-    }
-
-    protected void invoke(final Object[] targets) {
-        for (Object target : targets) {
-            invoke(target);
-        }
-    }
-
-    protected Class<Void> invoke(final Object target) {
-        final Method method = getMethod();
-        try {
-            method.invoke(target, getArguments());
-        } catch (IllegalArgumentException e) {
-            throw new PicoCompositionException("Can't call " + method.getName() + " on " + target, e);
-        } catch (IllegalAccessException e) {
-            throw new PicoCompositionException("Can't call " + method.getName() + " on " + target, e);
-        } catch (InvocationTargetException e) {
-            throw new PicoCompositionException("Failed when calling " + method.getName() + " on " + target, e
-                    .getTargetException());
-        }
-        return Void.TYPE;
-    }
+    return Void.TYPE;
+  }
 }
