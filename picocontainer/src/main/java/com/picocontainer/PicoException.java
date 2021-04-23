1baac6864fce5e8c2e8c4388c02448cf0797ee1b
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-23 12:10:58 +0200

    refactor: cleanup PicoException

--- picocontainer/src/main/java/com/picocontainer/PicoException.java
+++ picocontainer/src/main/java/com/picocontainer/PicoException.java
@@ -10,50 +10,31 @@
 package com.picocontainer;
 
 /**
- * Superclass for all Exceptions in PicoContainer. You can use this if you want to catch all exceptions thrown by
- * PicoContainer. Be aware that some parts of the PicoContainer API will also throw {@link NullPointerException} when
- * <code>null</code> values are provided for method arguments, and this is not allowed.
+ * <p>
+ * Superclass for all exceptions in PicoContainer.
+ * You can use this if you want to catch all exceptions thrown by PicoContainer.
+ * </p>
+ * <p>
+ * Be aware that some parts of the PicoContainer API will also throw {@link NullPointerException}
+ * when {@code null} values are provided for method arguments and this is not allowed.
+ * </p>
  *
  * @author Paul Hammant
  * @author Aslak Helles&oslash;y
  */
 @SuppressWarnings("serial")
 public abstract class PicoException extends RuntimeException {
+  protected PicoException() { }
 
-    /**
-     * Construct a new exception with no cause and no detail message. Note modern JVMs may still track the exception
-     * that caused this one.
-     */
-    protected PicoException() {
-    }
+  protected PicoException(final String message) {
+    super(message);
+  }
 
-    /**
-     * Construct a new exception with no cause and the specified detail message.  Note modern JVMs may still track the
-     * exception that caused this one.
-     *
-     * @param message the message detailing the exception.
-     */
-    protected PicoException(final String message) {
-        super(message);
-    }
+  protected PicoException(final Throwable cause) {
+    super(cause);
+  }
 
-    /**
-     * Construct a new exception with the specified cause and no detail message.
-     *
-     * @param cause the exception that caused this one.
-     */
-    protected PicoException(final Throwable cause) {
-        super(cause);
-    }
-
-    /**
-     * Construct a new exception with the specified cause and the specified detail message.
-     *
-     * @param message the message detailing the exception.
-     * @param cause   the exception that caused this one.
-     */
-    protected PicoException(final String message, final Throwable cause) {
-        super(message,cause);
-    }
-
+  protected PicoException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
 }
