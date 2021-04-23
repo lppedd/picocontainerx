e177ff4137cff0e18adea48a6850310f4b698d1e
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-23 14:17:18 +0200

    refactor: cleanup PicoCompositionException

--- picocontainer/src/main/java/com/picocontainer/PicoCompositionException.java
+++ picocontainer/src/main/java/com/picocontainer/PicoCompositionException.java
@@ -12,40 +12,22 @@ package com.picocontainer;
 
 /**
  * Subclass of {@link PicoException} that is thrown when there is:
- *   - a problem initializing the container
- *   - a cyclic dependency between components occurs.
- *   - problem adding a component
- *   - a request for a component that is ambiguous.
- *
+ * - a problem initializing the container
+ * - a cyclic dependency between components occurs.
+ * - problem adding a component
+ * - a request for a component that is ambiguous.
  */
 @SuppressWarnings("serial")
 public class PicoCompositionException extends PicoException {
-    /**
-     * Construct a new exception with no cause and the specified detail message.  Note modern JVMs may still track the
-     * exception that caused this one.
-     *
-     * @param message the message detailing the exception.
-     */
-    public PicoCompositionException(final String message) {
-        super(message);
-    }
+  public PicoCompositionException(final String message) {
+    super(message);
+  }
 
-    /**
-     * Construct a new exception with the specified cause and no detail message.
-     *
-     * @param cause the exception that caused this one.
-     */
-    public PicoCompositionException(final Throwable cause) {
-        super(cause);
-    }
+  public PicoCompositionException(final Throwable cause) {
+    super(cause);
+  }
 
-    /**
-     * Construct a new exception with the specified cause and the specified detail message.
-     *
-     * @param message the message detailing the exception.
-     * @param cause   the exception that caused this one.
-     */
-    public PicoCompositionException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
+  public PicoCompositionException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
 }
