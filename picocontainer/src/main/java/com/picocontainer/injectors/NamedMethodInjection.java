c6fa7d01f7ea3e756179ff3b6374e195025932a9
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-23 12:06:37 +0200

    refactor: cleanup NamedMethodInjection

--- picocontainer/src/main/java/com/picocontainer/injectors/NamedMethodInjection.java
+++ picocontainer/src/main/java/com/picocontainer/injectors/NamedMethodInjection.java
@@ -1,6 +1,11 @@
 package com.picocontainer.injectors;
 
-import com.picocontainer.*;
+import com.picocontainer.ComponentAdapter;
+import com.picocontainer.ComponentMonitor;
+import com.picocontainer.LifecycleStrategy;
+import com.picocontainer.NameBinding;
+import com.picocontainer.PicoContainer;
+import com.picocontainer.injectors.SetterInjection.SetterInjector;
 import com.picocontainer.parameters.ConstructorParameters;
 import com.picocontainer.parameters.FieldParameters;
 import com.picocontainer.parameters.MethodParameters;
@@ -12,76 +17,114 @@ import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
+/**
+ * TODO
+ */
+@SuppressWarnings("serial")
 public class NamedMethodInjection extends AbstractInjectionType {
+  private final String prefix;
+  private final boolean optional;
 
-    private final String prefix;
+  public NamedMethodInjection(final String prefix) {
+    this(prefix, true);
+  }
+
+  public NamedMethodInjection() {
+    this("set");
+  }
+
+  public NamedMethodInjection(final boolean optional) {
+    this("set", optional);
+  }
+
+  public NamedMethodInjection(final String prefix, final boolean optional) {
+    this.prefix = prefix;
+    this.optional = optional;
+  }
+
+  @Override
+  public <T> ComponentAdapter<T> createComponentAdapter(
+      final ComponentMonitor monitor,
+      final LifecycleStrategy lifecycle,
+      final Properties componentProps,
+      final Object key,
+      final Class<T> impl,
+      final ConstructorParameters constructorParams,
+      final FieldParameters[] fieldParams,
+      final MethodParameters[] methodParams) {
+    return wrapLifeCycle(
+        monitor.newInjector(new NamedMethodInjector<>(key, impl, monitor, prefix, optional, methodParams)),
+        lifecycle
+    );
+  }
+
+  @SuppressWarnings("serial")
+  public static class NamedMethodInjector<T> extends SetterInjector<T> {
     private final boolean optional;
 
-    public NamedMethodInjection(final String prefix) {
-        this(prefix, true);
+    public NamedMethodInjector(
+        final Object key,
+        final Class<T> impl,
+        final ComponentMonitor monitor,
+        final boolean optional,
+        final MethodParameters... parameters) {
+      this(key, impl, monitor, "set", optional, parameters);
     }
 
-    public NamedMethodInjection() {
-        this("set");
+    public NamedMethodInjector(
+        final Object key,
+        final Class<T> impl,
+        final ComponentMonitor monitor,
+        final MethodParameters... parameters) {
+      this(key, impl, monitor, "set", true, parameters);
     }
 
-    public NamedMethodInjection(final boolean optional) {
-        this("set", optional);
+    public NamedMethodInjector(
+        final Object key,
+        final Class<T> impl,
+        final ComponentMonitor monitor,
+        final String prefix,
+        final MethodParameters... parameters) {
+      this(key, impl, monitor, prefix, true, parameters);
     }
 
-    public NamedMethodInjection(final String prefix, final boolean optional) {
-        this.prefix = prefix;
-        this.optional = optional;
+    public NamedMethodInjector(
+        final Object key,
+        final Class<T> impl,
+        final ComponentMonitor monitor,
+        final String prefix,
+        final boolean optional,
+        final MethodParameters... parameters) {
+      super(key, impl, monitor, prefix, true, "", false, parameters);
+      this.optional = optional;
     }
 
-    public <T> ComponentAdapter<T> createComponentAdapter(final ComponentMonitor monitor, final LifecycleStrategy lifecycle, final Properties componentProps, final Object key, final Class<T> impl, final ConstructorParameters constructorParams, final FieldParameters[] fieldParams, final MethodParameters[] methodParams) throws PicoCompositionException {
-        return wrapLifeCycle(monitor.newInjector(new NamedMethodInjector(key, impl, monitor, prefix, optional, methodParams)), lifecycle);
+    @Override
+    protected NameBinding makeParameterNameImpl(final AccessibleObject member) {
+      return () -> {
+        final String name = ((Method) member).getName().substring(prefix.length()); // string off 'set' or chosen prefix
+        return name.substring(0, 1).toLowerCase() + name.substring(1);  // change "SomeThing" to "someThing"
+      };
     }
 
-    @SuppressWarnings("serial")
-    public static class NamedMethodInjector<T> extends SetterInjection.SetterInjector<T> {
-
-        private final boolean optional;
-
-        public NamedMethodInjector(final Object key, final Class<T> impl, final ComponentMonitor monitor, final boolean optional, final MethodParameters... parameters) {
-            this(key, impl, monitor, "set", optional, parameters);
-        }
-
-        public NamedMethodInjector(final Object key, final Class<T> impl, final ComponentMonitor monitor, final MethodParameters... parameters) {
-            this(key, impl, monitor, "set", true, parameters);
-        }
-
-        public NamedMethodInjector(final Object key, final Class<T> impl, final ComponentMonitor monitor, final String prefix, final MethodParameters... parameters) {
-            this(key, impl, monitor, prefix, true, parameters);
-        }
-
-        public NamedMethodInjector(final Object key, final Class<T> impl, final ComponentMonitor monitor, final String prefix, final boolean optional, final MethodParameters... parameters) {
-            super(key, impl, monitor, prefix, true, "", false, parameters);
-            this.optional = optional;
-        }
-
-        @Override
-        protected NameBinding makeParameterNameImpl(final AccessibleObject member) {
-            return new NameBinding() {
-                public String getName() {
-                    String name = ((Method)member).getName().substring(prefix.length()); // string off 'set' or chosen prefix
-                    return name.substring(0,1).toLowerCase() + name.substring(1);  // change "SomeThing" to "someThing"
-                }
-            };
-        }
-
-        @Override
-        protected void unsatisfiedDependencies(final PicoContainer container, final Set<Type> unsatisfiableDependencyTypes, final List<AccessibleObject> unsatisfiableDependencyMembers) {
-            if (!optional) {
-                throw new UnsatisfiableDependenciesException(this.getComponentImplementation().getName() + " has unsatisfied dependencies " + unsatisfiableDependencyTypes
-                        + " for members " + unsatisfiableDependencyMembers + " from " + container);
-            }
-        }
-
-        @Override
-        public String getDescriptor() {
-            return "NamedMethodInjection";
-        }
-
+    @Override
+    protected void unsatisfiedDependencies(
+        final PicoContainer container,
+        final Set<Type> unsatisfiableDependencyTypes,
+        final List<AccessibleObject> unsatisfiableDependencyMembers) {
+      if (!optional) {
+        throw new UnsatisfiableDependenciesException(
+            getComponentImplementation().getName()
+                + " has unsatisfied dependencies " + unsatisfiableDependencyTypes
+                + " for members " + unsatisfiableDependencyMembers
+                + " from " + container
+        );
+      }
     }
+
+    @Override
+    public String getDescriptor() {
+      return "NamedMethodInjection";
+    }
+  }
 }
