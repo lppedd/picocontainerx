4c94f9d256ad0e788b1bc112ec60ed7f52b1a0cb
Edoardo Luppi <lp.edoardo@gmail.com>
2021-04-23 11:56:12 +0200

    refactor: cleanup MultiInjection

--- picocontainer/src/main/java/com/picocontainer/injectors/MultiInjection.java
+++ picocontainer/src/main/java/com/picocontainer/injectors/MultiInjection.java
@@ -8,9 +8,17 @@
  *****************************************************************************/
 package com.picocontainer.injectors;
 
-import com.picocontainer.*;
+import com.picocontainer.Characteristics;
+import com.picocontainer.ComponentAdapter;
+import com.picocontainer.ComponentMonitor;
+import com.picocontainer.LifecycleStrategy;
 import com.picocontainer.annotations.Inject;
 import com.picocontainer.behaviors.AbstractBehavior;
+import com.picocontainer.injectors.AnnotatedFieldInjection.AnnotatedFieldInjector;
+import com.picocontainer.injectors.AnnotatedMethodInjection.AnnotatedMethodInjector;
+import com.picocontainer.injectors.CompositeInjection.CompositeInjector;
+import com.picocontainer.injectors.ConstructorInjection.ConstructorInjector;
+import com.picocontainer.injectors.SetterInjection.SetterInjector;
 import com.picocontainer.parameters.ConstructorParameters;
 import com.picocontainer.parameters.FieldParameters;
 import com.picocontainer.parameters.MethodParameters;
@@ -19,50 +27,79 @@ import java.util.Properties;
 
 import static com.picocontainer.injectors.AnnotatedMethodInjection.getInjectionAnnotation;
 
-/** @author Paul Hammant */
+/**
+ * @author Paul Hammant
+ */
 @SuppressWarnings("serial")
 public class MultiInjection extends AbstractInjectionType {
-    private final String setterPrefix;
+  private final String setterPrefix;
 
-    public MultiInjection(final String setterPrefix) {
-        this.setterPrefix = setterPrefix;
+  public MultiInjection(final String setterPrefix) {
+    this.setterPrefix = setterPrefix;
+  }
+
+  public MultiInjection() {
+    this("set");
+  }
+
+  @Override
+  public <T> ComponentAdapter<T> createComponentAdapter(
+      final ComponentMonitor monitor,
+      final LifecycleStrategy lifecycle,
+      final Properties componentProps,
+      final Object key,
+      final Class<T> impl,
+      final ConstructorParameters constructorParams,
+      final FieldParameters[] fieldParams,
+      final MethodParameters[] methodParams) {
+    final boolean useNames =
+        AbstractBehavior.arePropertiesPresent(componentProps, Characteristics.USE_NAMES, true);
+    final boolean requireConsumptionOfAllParameters =
+        !AbstractBehavior.arePropertiesPresent(componentProps, Characteristics.ALLOW_UNUSED_PARAMETERS, false);
+
+    return wrapLifeCycle(
+        new MultiInjector<>(
+            key,
+            impl,
+            monitor,
+            setterPrefix,
+            useNames,
+            requireConsumptionOfAllParameters,
+            constructorParams,
+            fieldParams,
+            methodParams
+        ),
+        lifecycle
+    );
+  }
+
+  /**
+   * @author Paul Hammant
+   */
+  @SuppressWarnings("serial")
+  public static class MultiInjector<T> extends CompositeInjector<T> {
+    @SuppressWarnings("unchecked")
+    public MultiInjector(
+        final Object key,
+        final Class<T> impl,
+        final ComponentMonitor monitor,
+        final String setterPrefix,
+        final boolean useNames,
+        final boolean useAllParameter,
+        final ConstructorParameters constructorParams,
+        final FieldParameters[] fieldParams,
+        final MethodParameters[] methodParams) {
+      super(key, impl, monitor, useNames,
+          monitor.newInjector(new ConstructorInjector<>(monitor, useNames, key, impl, constructorParams)),
+          monitor.newInjector(new SetterInjector<>(key, impl, monitor, setterPrefix, useNames, "", false, methodParams)),
+          monitor.newInjector(new AnnotatedMethodInjector<>(key, impl, methodParams, monitor, useNames, useAllParameter, Inject.class, getInjectionAnnotation("javax.inject.Inject"))),
+          monitor.newInjector(new AnnotatedFieldInjector<>(key, impl, fieldParams, monitor, useNames, useAllParameter, Inject.class, getInjectionAnnotation("javax.inject.Inject")))
+      );
     }
 
-    public MultiInjection() {
-        this("set");
+    @Override
+    public String getDescriptor() {
+      return "MultiInjector";
     }
-
-    public <T> ComponentAdapter<T> createComponentAdapter(final ComponentMonitor monitor,
-                                                          final LifecycleStrategy lifecycle,
-                                                          final Properties componentProps,
-                                                          final Object key,
-                                                          final Class<T> impl,
-                                                          final ConstructorParameters constructorParams, final FieldParameters[] fieldParams, final MethodParameters[] methodParams) throws PicoCompositionException {
-        boolean useNames = AbstractBehavior.arePropertiesPresent(componentProps, Characteristics.USE_NAMES, true);
-        boolean requireConsumptionOfAllParameters = !(AbstractBehavior.arePropertiesPresent(componentProps, Characteristics.ALLOW_UNUSED_PARAMETERS, false));
-
-        return wrapLifeCycle(new MultiInjector<T>(key, impl, monitor, setterPrefix, useNames, requireConsumptionOfAllParameters, constructorParams, fieldParams, methodParams), lifecycle);
-    }
-
-    /** @author Paul Hammant */
-    @SuppressWarnings("serial")
-    public static class MultiInjector<T> extends CompositeInjection.CompositeInjector<T> {
-
-        @SuppressWarnings("unchecked")
-		public MultiInjector(final Object key, final Class<T> impl, final ComponentMonitor monitor, final String setterPrefix, final boolean useNames, final boolean useAllParameter,
-        		final ConstructorParameters constructorParams, final FieldParameters[] fieldParams, final MethodParameters[] methodParams) {
-            super(key, impl, monitor, useNames,
-                    monitor.newInjector(new ConstructorInjection.ConstructorInjector<T>(monitor, useNames, key, impl, constructorParams)),
-                    monitor.newInjector(new SetterInjection.SetterInjector<T>(key, impl, monitor, setterPrefix, useNames, "", false, methodParams)),
-                    monitor.newInjector(new AnnotatedMethodInjection.AnnotatedMethodInjector<T>(key, impl, methodParams, monitor, useNames, useAllParameter, Inject.class, getInjectionAnnotation("javax.inject.Inject"))),
-                    monitor.newInjector(new AnnotatedFieldInjection.AnnotatedFieldInjector<T>(key, impl, fieldParams, monitor, useNames, useAllParameter, Inject.class, getInjectionAnnotation("javax.inject.Inject")))
-           );
-
-        }
-
-        @Override
-		public String getDescriptor() {
-            return "MultiInjector";
-        }
-    }
+  }
 }
